54d7d03b333bb04fd603293096c09671ef82f12b
qinkaiyuan <qinkaiyuan94@163.com>
2021-08-07 01:01:53 +0800

    支持抱抱

--- social-sdk/src/main/java/com/socialuni/sdk/service/SocialHugService.java
+++ social-sdk/src/main/java/com/socialuni/sdk/service/SocialHugService.java
@@ -6,11 +6,11 @@ import com.socialuni.sdk.model.DO.user.UserDO;
 import com.socialuni.sdk.utils.SocialUserUtil;
 import com.socialuni.social.model.model.QO.community.SocialHugAddQO;
 import com.socialuni.social.model.model.RO.ResultRO;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@RestController
+@Service
 public class SocialHugService {
     @Resource
     private SocialAddHugDomain socialAddHugDomain;
