15d1b64dc2f24f7bf5c745fae72a43a4d6a07b41
qinkaiyuan <qinkya@163.com>
2021-08-10 13:32:12 +0800

    提交清池获取用户信息相关代码

--- social-sdk/src/main/java/com/socialuni/sdk/store/SocialUserAccountStore.java
+++ social-sdk/src/main/java/com/socialuni/sdk/store/SocialUserAccountStore.java
@@ -1,12 +1,11 @@
 package com.socialuni.sdk.store;
 
-import com.socialuni.social.model.model.QO.user.SocialProviderLoginQO;
 import com.socialuni.sdk.constant.ProviderLoginType;
-import com.socialuni.sdk.exception.SocialParamsException;
-import com.socialuni.sdk.model.DO.user.SocialUserAccountDO;
-import com.socialuni.sdk.repository.SocialUserAccountRepository;
 import com.socialuni.sdk.constant.platform.PlatformType;
+import com.socialuni.sdk.model.DO.user.SocialUserAccountDO;
 import com.socialuni.sdk.model.UniUnionIdRO;
+import com.socialuni.sdk.repository.SocialUserAccountRepository;
+import com.socialuni.social.model.model.QO.user.SocialProviderLoginQO;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -40,9 +39,10 @@ public class SocialUserAccountStore {
     public SocialUserAccountDO getSocialAccountByUnionId(String unionId) {
         SocialUserAccountDO socialUserAccountDO = socialUserAccountRepository.findByProviderAndUnionId(ProviderLoginType.socialuni, unionId);
         //根据unionId找不到，代表没有开通开放平台或者没注册
-        if (socialUserAccountDO == null) {
+        //目前只有查询用户详情使用，查询这个社交平台的用户是否在本平台注册了，在本平台注册，则使用本平台的用户信息
+        /*if (socialUserAccountDO == null) {
             throw new SocialParamsException("不存在的用户");
-        }
+        }*/
         return socialUserAccountDO;
     }
 }
