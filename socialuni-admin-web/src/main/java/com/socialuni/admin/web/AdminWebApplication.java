d3eb88ededdd5fabf1261f82116b3c10829aaa48
qinkaiyuan <qinkaiyuan94@163.com>
2021-08-14 14:31:01 +0800

    支持向开发环境同步开发者账号接口

--- socialuni-admin-web/src/main/java/com/socialuni/admin/web/AdminWebApplication.java
+++ socialuni-admin-web/src/main/java/com/socialuni/admin/web/AdminWebApplication.java
@@ -1,15 +1,10 @@
 package com.socialuni.admin.web;
 
+import com.socialuni.api.config.EnableSocialuni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EntityScan("com.qingchi")
-@EnableJpaRepositories("com.qingchi")
-@ComponentScan(value="com.qingchi")
 @EnableSocialuni
 @EnableTransactionManagement
 @SpringBootApplication
