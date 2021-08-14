e41c657c464e4d849e229d58759ed42942e2ca71
qinkaiyuan <qinkaiyuan94@163.com>
2021-08-14 20:43:59 +0800

    支持三方授权

--- src/pages/user/qcloginNew.vue
+++ src/pages/user/qcloginNew.vue
@@ -6,10 +6,13 @@
     <div class="pt-6vh pb-sm px w100p">
       <div class="flex-none col-row-center h80px">
         <div class="text-xxl font-bold">欢迎使用社交联盟授权登录</div>
+        <div v-if="showPhoneView" class="text-md u-type-warning mt-sm">建议使用微信 或 QQ一键登录</div>
       </div>
 
       <view class="mt h150px">
-        <view class="h100p row-center pb-md">
+        <phone-login-form v-if="showPhoneView" :phone-form-data="phoneFormData"></phone-login-form>
+
+        <view v-else class="h100p row-center pb-md">
           <!--          头部-->
 
           <!--          只有不为三方授权才显示logo-->
@@ -33,6 +36,7 @@
       <!--        隐私提示-->
       <user-privacy-agreement></user-privacy-agreement>
 
+
       <!--    返回和登录方式切换-->
       <!--      这个div是为了处理居中问题-->
       <div>
@@ -43,14 +47,47 @@
               <!--              没登录提示登录，如果为三方授权且为授权用户信息，追加 并授权三个字-->
               <!-- 只要不为QQ小程序平台都可以使用微信登录-->
               <template v-if="!user">
-                <!--                app和h5也都可以用微信登录-->
-                <button :disabled="!openTypeBtnEnable"
+                <button v-if="showPhoneView" :disabled="loginButtonDisabled" @click="phoneLogin"
+                        class="h40px cu-btn lg bg-gradual-phone  row-all-center bd-none bg-active round mt w100p"
+                >
+                  <u-icon custom-prefix="mdi" color="white" name="cellphone-android" size="42" class="mr-xs"></u-icon>
+                  手机号登录
+                </button>
+                <button v-else-if="isMpQQ" :disabled="!openTypeBtnEnable"
+                        open-type="getUserInfo"
                         class="bg-gradual-qq h40px cu-btn lg row-all-center bd-none bg-active round mt w100p"
-                        @click="socialUniAuthLogin">
-                  跳转清池授权登录
+                        @getuserinfo="providerLogin">
+                  <u-icon color="white" name="weixin-fill" size="42"
+                          class="mr-xs"></u-icon>
+                  QQ登录
+                </button>
+                <!--                app和h5也都可以用微信登录-->
+                <button v-else :disabled="!openTypeBtnEnable"
+                        class="bg-gradual-wx h40px cu-btn lg row-all-center bd-none bg-active round mt w100p"
+                        @click="providerLogin">
+                  <u-icon color="white" name="weixin-fill" size="42"
+                          class="mr-xs"></u-icon>
+                  微信登录
                 </button>
               </template>
               <!--              有用户-->
+              <template v-else>
+                <button v-if="showPhoneView" :disabled="loginButtonDisabled" @click="bindPhoneNum"
+                        class="h40px cu-btn lg bg-gradual-phone  row-all-center bd-none bg-active round mt w100p"
+                >
+                  <u-icon custom-prefix="mdi" color="white" name="cellphone-android" size="42" class="mr-xs"></u-icon>
+                  绑定手机号
+                </button>
+
+                <button v-else-if="isMpWx" :disabled="!openTypeBtnEnable"
+                        open-type="getPhoneNumber"
+                        class="h40px cu-btn lg bg-gradual-wx row-all-center bd-none bg-active round mt w100p"
+                        @getphonenumber="bindWxPhoneNum">
+                  <u-icon color="white" name="weixin-fill" size="42"
+                          class="mr-xs"></u-icon>
+                  绑定微信手机号
+                </button>
+              </template>
             </template>
           </view>
 
@@ -60,6 +97,20 @@
               <view class="text-gray u-border-bottom">
                 {{ user ? '不绑定返回' : '不登录返回' }}
               </view>
+            </view>
+
+            <view @click="switchShowPhoneNum" class="row-end-center">
+              <view class="text-gray">
+                <template v-if="!user">
+                  {{ showPhoneView ? '其他方式登录' : '手机号登录' }}
+                </template>
+                <template v-else-if="user && isMpWx">
+                  {{ showPhoneView ? '其他方式绑定' : '验证码方式绑定' }}
+                </template>
+              </view>
+              <!--              验证码登录、或者没用户、或者没手机号且不为授权用户、-->
+              <u-icon class="ml-xs text-gray"
+                      name="arrow-right"></u-icon>
             </view>
           </view>
         </view>
@@ -75,8 +126,10 @@
 import { Component, Vue } from 'vue-property-decorator'
 import UserVO from '@/model/user/UserVO'
 import { namespace } from 'vuex-class'
-import { appModule, systemModule } from '@/store'
+import { systemModule } from '@/store'
 import Alert from '@/utils/Alert'
+import Constants from '@/const/Constant'
+import Toast from '@/utils/Toast'
 import LoginService from '@/service/LoginService'
 import PageUtil from '@/utils/PageUtil'
 import SystemStoreProp from '@/store/SystemStoreProp'
@@ -86,9 +139,6 @@ import UserPrivacyAgreement from '@/pages/user/UserPrivacyAgreement.vue'
 import PhoneNumFormData from '@/model/phone/PhoneNumFormData'
 import PhoneLoginForm from '@/pages/user/PhoneLoginForm.vue'
 import PhoneService from '@/service/PhoneService'
-import UserService from '@/service/UserService'
-import SocialUniAuthVO from '@/model/openData/SocialUniAuthVO'
-import MockAPI from '@/api/MockAPI'
 
 const userStore = namespace('user')
 const configStore = namespace('config')
@@ -160,33 +210,19 @@ export default class LoginPage extends Vue {
     }
   }
 
-  socialUniAuthLogin () {
-    if (this.openTypeBtnEnable) {
-      try {
-        this.openTypeBtnEnable = false
-        //开发模式模拟授权
-        if (appModule.isDevMode) {
-          MockAPI.mockOAuthUserInfoAPI().then(res => {
-            UserService.getMineUserInitDataActionByToken(res.data)
-            PageUtil.toTalkPage()
-            this.loginAfterHint('登录成功')
-          })
-        } else {
-          const authVO: SocialUniAuthVO = new SocialUniAuthVO('user')
-          PageUtil.toSocialUniAuth(authVO)
-        }
-      } finally {
-        this.openTypeBtnEnable = true
-      }
-    }
-  }
-
   //平台登录
   //登录，授权，绑定手机号各大平台登录结果，后者授权手机号结果
   async providerLogin (result) {
     if (this.openTypeBtnEnable) {
       try {
         this.openTypeBtnEnable = false
+        if (systemModule.isMp) {
+          if (systemModule.isMpQQ) {
+            if (result.detail.errMsg !== Constants.loginSuccess) {
+              return Toast.toast('您取消了登录')
+            }
+          }
+        }
         //一行代码就可以获取登录所需要的信息, 还可以配合后台使用，一键登录，记住用户
         await LoginService.providerLogin(systemModule.mpPlatform)
         this.loginAfterHint('登录成功')
