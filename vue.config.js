8a04ceca12f4e3e5dac85a067a434a9b22eaea83
qinkaiyuan <qinkaiyuan94@163.com>
2021-08-15 21:36:51 +0800

    支持举报功能

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     // 防止控制台报错 localhost:8080无法连接
-    port: 8090,
+    port: 8025,
     disableHostCheck: true
   },
   transpileDependencies: ['@dcloudio/uni-ui']
