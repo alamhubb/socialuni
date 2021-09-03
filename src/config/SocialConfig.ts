e4999d9261dc9d1475163627807db5de76b31059
qinkaiyuan <qinkaiyuan94@163.com>
2021-09-03 21:20:30 +0800

    修复cdn问题

--- src/config/SocialConfig.ts
+++ src/config/SocialConfig.ts
@@ -1,7 +1,7 @@
 import GenderType from '@/const/GenderType'
 
 export default class SocialConfig {
-  static readonly socialSecretKey = process.env.VUE_APP_SOCIAL_SECRET_KEY
+  static readonly socialSecretKey = process.env.VUE_APP_SOCIALUNI_SECRET_KEY
   //app的性别类型，全部性别，还是仅女生可用
   static readonly appGenderType = GenderType.all
   static readonly authApp = true
