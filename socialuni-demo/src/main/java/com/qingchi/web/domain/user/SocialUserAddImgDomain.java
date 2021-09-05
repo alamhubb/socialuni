280747064cf3d00776ed67c5cc61701e819f9f6c
qinkaiyuan <qinkaiyuan94@163.com>
2021-09-05 19:23:16 +0800

    支持首次创建返回秘钥

--- socialuni-demo/src/main/java/com/qingchi/web/domain/user/SocialUserAddImgDomain.java
+++ socialuni-demo/src/main/java/com/qingchi/web/domain/user/SocialUserAddImgDomain.java
@@ -1,26 +1,11 @@
 package com.qingchi.web.domain.user;
 
-import com.qingchi.web.model.DO.user.IdentityImgDO;
-import com.socialuni.api.model.RO.user.CenterMineUserDetailRO;
-import com.socialuni.social.api.model.ResultRO;
-import com.socialuni.social.constant.ErrorCode;
-import com.socialuni.social.entity.model.DO.user.UserDO;
-import com.socialuni.social.entity.model.DO.user.UserImgDO;
-import com.socialuni.social.exception.SocialBusinessException;
-import com.socialuni.social.model.model.RO.user.SocialMineUserDetailRO;
-import com.socialuni.social.sdk.constant.ErrorMsg;
-import com.socialuni.social.sdk.constant.status.UserStatus;
-import com.socialuni.social.sdk.factory.user.img.UserImgDOFactory;
-import com.socialuni.social.model.model.QO.user.SocialUserImgAddQO;
-import com.socialuni.social.sdk.platform.tencent.TencentCloud;
 import com.socialuni.social.sdk.repository.UserRepository;
 import com.socialuni.social.sdk.service.comment.IllegalWordService;
-import com.socialuni.social.sdk.utils.model.DO.UserImgDOUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Component
 @Slf4j
