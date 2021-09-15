e22d6888ea9acd6373f49302e13beca19561d72d
qinkaiyuan <qinkaiyuan94@163.com>
2021-09-16 00:20:11 +0800

    演示系统支持登录

--- socialuni-api/src/main/java/com/socialuni/api/feignAPI/SocialuniMockOAuthAPI.java
+++ socialuni-api/src/main/java/com/socialuni/api/feignAPI/SocialuniMockOAuthAPI.java
@@ -4,6 +4,8 @@ import com.socialuni.api.model.RO.user.CenterMineUserDetailRO;
 import com.socialuni.api.model.RO.user.CenterUserDetailRO;
 import com.socialuni.social.api.model.ResultRO;
 import com.socialuni.social.model.model.QO.MockQO;
+import com.socialuni.social.model.model.QO.user.MockOAuthUserInfoQO;
+import com.socialuni.social.model.model.QO.user.OAuthUserInfoQO;
 import com.socialuni.social.model.model.RO.user.login.SocialLoginRO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(name = "mock", url = "${socialuni.server-url:https://api.socialuni.cn}")
 public interface SocialuniMockOAuthAPI {
     @PostMapping("mockOAuthUserInfo")
-    ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserInfo();
+    ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserInfo(@RequestBody MockOAuthUserInfoQO authVO);
 
 
     @PostMapping("mockOAuthUserPhoneNum")
-    ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserPhoneNum();
+    ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserPhoneNum(@RequestBody MockOAuthUserInfoQO authVO);
 }
--- socialuni-center-web/src/main/java/com/socialuni/center/web/controller/CenterMockOAuthController.java
+++ socialuni-center-web/src/main/java/com/socialuni/center/web/controller/CenterMockOAuthController.java
@@ -5,6 +5,7 @@ import com.socialuni.api.feignAPI.SocialuniMockOAuthAPI;
 import com.socialuni.api.model.RO.user.CenterMineUserDetailRO;
 import com.socialuni.center.web.serive.CenterMockService;
 import com.socialuni.social.api.model.ResultRO;
+import com.socialuni.social.model.model.QO.user.MockOAuthUserInfoQO;
 import com.socialuni.social.model.model.RO.user.login.SocialLoginRO;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +17,13 @@ public class CenterMockOAuthController implements SocialuniMockOAuthAPI {
     CenterMockService centerMockService;
 
     @Override
-    public ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserInfo() {
+    public ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserInfo(MockOAuthUserInfoQO authVO) {
         ResultRO<SocialLoginRO<CenterMineUserDetailRO>> resultRO = centerMockService.mockOAuthUserInfo();
         return resultRO;
     }
 
     @Override
-    public ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserPhoneNum() {
+    public ResultRO<SocialLoginRO<CenterMineUserDetailRO>> mockOAuthUserPhoneNum(MockOAuthUserInfoQO authVO) {
         return centerMockService.mockOAuthUserPhoneNum();
     }
 }
