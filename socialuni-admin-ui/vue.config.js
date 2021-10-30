9c82ec7dba3a3d7deb7fedcff5f14314d416e868
qinkaiyuan <qinkaiyuan94@163.com>
2021-10-31 01:10:47 +0800

    支持修改开发者信息

--- socialuni-admin-ui/vue.config.js
+++ socialuni-admin-ui/vue.config.js
@@ -1,3 +1,15 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const cdn = {
+  css: [],
+  js: [
+    // vue
+    'https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js',
+    'https://cdn.bootcss.com/vue-router/3.2.0/vue-router.min.js',
+    'https://cdn.bootcss.com/vuex/3.6.2/vuex.min.js',
+    'https://cdn.bootcss.com/axios/0.19.2/axios.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/element-ui/2.14.1/index.min.js'
+  ]
+}
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
@@ -20,6 +32,27 @@ module.exports = {
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
         }
+      }
+    }
+  },
+  chainWebpack: config => {
+    if (isProduction) {
+      config.entry('app').clear().add('./src/main-prod.ts')
+      config.externals({
+        'vue': 'Vue',
+        'vue-router': 'VueRouter',
+        'vuex': 'Vuex',
+        'axios': 'axios',
+        'element-ui': 'Element'
+      })
+      config.plugin('html')
+        .tap(args => {
+          args[0].cdn = cdn
+          return args
+        })
+    } else {
+      config => {
+        config.entry('app').clear().add('./src/main-dev.ts')
       }
     }
   }
