94e580bef4f0f3305c43f0964ec4f87513be8cf4
qinkaiyuan <socialuni@163.com>
2022-01-22 00:34:08 +0800

    整理白名单url格式

--- socialuni-center-web/src/main/java/com/socialuni/center/web/config/WebInterceptor.java
+++ socialuni-center-web/src/main/java/com/socialuni/center/web/config/WebInterceptor.java
@@ -78,96 +78,19 @@ public class WebInterceptor extends SocialWebRequestLogInterceptor {
             }
         }
 
-        if (
-                (requestMethod.equals(RequestMethod.OPTIONS.name())
-                        || uri.equals("/")
-                        || uri.contains("test")
-                        //初始化
-                        || uri.contains("getAppLaunchData")
-                        //初始化的请求
-                        || uri.contains("report/queryReportTypes")
-                        //查询首页轮播图
-                        || uri.contains("queryHomeSwipers")
-                        //同步生产环境数据到开发
-                        || uri.contains("queryDevAccount")
-                        || uri.contains("queryHotCircles")
-                        || uri.contains("queryCircleTypes")
-                        || uri.contains("admin/syncProdDevAccount")
-                        //mock授权登录
-                        || uri.contains("mockOAuthUserInfo")
-                        || uri.contains("mockOAuthUserPhoneNum")
-                        || uri.contains("tag/queryTags")
-                        || uri.contains("tag/queryHotTags")
-                        || uri.contains("tag/queryTagTypes")
-                        || uri.contains("tag/queryHotTagType")
-                        || uri.contains("/queryDistricts")
-                        || uri.contains("/queryHotDistricts")
-                        || uri.contains("/devUser/queryDevUserDetail")
-                        || uri.contains("/app/sendErrorLog")
-                        || uri.contains("/app/getAppConfig")
-                        //渠道登录的方法
-                        || uri.contains("queryProvinceDistricts")
-                        || uri.contains("queryHotProvinceDistricts")
-                        || uri.contains("queryOtherHomeTypeTalks")
-                        || uri.contains("readChat")
-                        || uri.contains("queryChats")
-                        || uri.contains("refreshKeywords")
-                        || uri.contains("refreshConfigMap")
-                        || uri.contains("refreshRedis")
-                        || uri.contains("qqPayNotify")
-                        || uri.contains("wxPayNotify")
-                        || uri.contains("app/checkUpdate")
-                        || uri.contains("match/queryMatchUsers")
-                        || uri.contains("authentication")
-                        || uri.contains("match/queryUsers")
-                        || uri.contains("talk/queryTalks")
-                        || uri.contains("message/queryMessages")
-                        || uri.contains("/webSocketServer")
-                        || uri.contains("/myHandler")
-                        || uri.contains("/queryUserTalks")
-                        || uri.contains("/queryTalkDetail")
-                        || uri.contains("/queryUserDetail")
-                        || uri.contains("/queryAppInitData")
-                        || uri.contains("/queryAppInitDataLoad")
-                        || uri.contains("/queryAppInitDataLoad1")
-                        || uri.contains("/queryAppInitDataReady")
-                        || uri.contains("/img")
-                        || uri.contains("/css")
-                        //这里只查询没被封禁的
-                        //有些接口必须有用户才可访问
-                        || user != null
-                        //有些接口必须有开发者账号才可访问
-                        //或者为这些url，但divId不为null
-                        || ((uri.contains("user/providerLogin")
-                        || uri.contains("user/platformLogin")
-                        || uri.contains("user/miniAppLogin")
-                        || uri.contains("user/appLogin")
-                        || uri.contains("appLogin")
-                        || uri.contains("wxLogin")
-                        || uri.contains("bindWxPhoneNum")
-                        || uri.contains("bindPhoneNum")
-                        || uri.contains("sendAuthCode")
-                        || uri.contains("login")
-                        || uri.contains("Login")
-                        || uri.contains("location")
-                        || uri.contains("oauth"))
-                        && devId != null)
-                )
-                        //不允许读取idcard信息
-                        && !uri.contains("/img/idCard")
-        ) {
+        if (user != null) {
             return true;
         }
-        /*else {
-            String origin = request.getHeader("Origin");
-            res.setHeader("Access-Control-Allow-Origin", origin);
-            res.setHeader("Access-Control-Allow-Methods", "*");
-            res.setHeader("Access-Control-Allow-Headers", "Origin,Content-Type,Accept,token,X-Requested-With");
-            res.setHeader("Access-Control-Allow-Credentials", "true");
-            //这里只提示未登录
-            res.setStatus(ErrorCode.NOT_LOGGED_ERROR);
-            return false;
-        }*/
+        for (String whiteUrl : WhiteUrlListConfig.AppWhiteUrlList) {
+            if (uri.contains(whiteUrl)) {
+                return true;
+            }
+        }
+        for (String whiteUrl : WhiteUrlListConfig.needDevIdUrlList) {
+            if (uri.contains(whiteUrl) && devId != null) {
+                return true;
+            }
+        }
         if (devId == null) {
             log.info("开发者为空");
             throw new SocialNullDevAccountException();
