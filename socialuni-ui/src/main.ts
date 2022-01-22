192b52e26f197e25e55036ad4de82bc6966ff535
qinkaiyuan <socialuni@163.com>
2022-01-22 19:00:54 +0800

    vue3+uniapp

--- socialuni-ui/src/main.ts
+++ socialuni-ui/src/main.ts
@@ -1,16 +1,14 @@
-import {createApp} from 'vue'
+import {createSSRApp} from 'vue'
 import {createPinia} from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/styles/qing-scss/index.scss'
 
-const app = createApp(App)
+const app = createSSRApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(ElementPlus)
 
 app.mount('#app')
