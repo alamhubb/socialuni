b215f0327c2a32036f72670047844ac378f2768d
qinkaiyuan <qinkaiyuan94@163.com>
2022-01-27 15:01:49 +0800

    移出 vuetify 相关功能

--- socialuni-pc/src/main.ts
+++ socialuni-pc/src/main.ts
@@ -1,20 +1,14 @@
-import {createApp} from "vue";
-import {createPinia} from "pinia";
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from "./App.vue";
-import router from "./router";
+import App from "./App.vue"
+import router from "./router"
 // import "@/styles/qing-scss/index.scss";
 // import "@/styles/index.scss";
-import vuetify from "@/plugins/vuetify";
-import {loadFonts} from "@/plugins/webfontloader";
 
 const app = createApp(App);
 
-
-loadFonts();
-
 app.use(createPinia());
 app.use(router);
-app.use(vuetify);
 
 app.mount("#app");
