e513c11430019fd64bad2cecebe14d0667b0a677
qinkaiyuan <socialuni@163.com>
2022-03-21 22:28:02 +0800

    修复圈子版本小程序的问题

--- socialuni-js/src/socialuni/components/SocialLogin/LoginFooterAppInfo.vue
+++ socialuni-js/src/socialuni/components/SocialLogin/LoginFooterAppInfo.vue
@@ -7,7 +7,7 @@
     </navigator>
 
     <view @click="copyServiceNum" class="flex-row u-type-info u-border-bottom">
-      客服微信、QQ：{{ qqService }}
+      客服{{ isMpQQ ? 'QQ' : '微信' }}：{{ qqService }}
       <q-icon icon="attach" size="15" class="ml-xs"></q-icon>
     </view>
   </div>
@@ -18,14 +18,16 @@ import { Component, Vue } from 'vue-property-decorator'
 import UniUtil from '../../utils/UniUtil'
 import ConfigMap from '../../constant/ConfigMap'
 import SkipUrlConst from '../../constant/SkipUrlConst'
-import { socialConfigStore } from '../../store'
+import { socialConfigStore, socialSystemStore } from '../../store'
 import QIcon from '@/qing-ui/components/QIcon/QIcon.vue'
+import SystemStoreProp from '@/socialuni/store/SystemStoreProp'
 
 @Component({
   components: { QIcon }
 })
 export default class LoginFooterAppInfo extends Vue {
   @socialConfigStore.Getter(ConfigMap.qqServiceKey) qqService: string
+  @socialSystemStore.State(SystemStoreProp.isMpQQ) isMpQQ: boolean
 
   get homeUrl (): string {
     return SkipUrlConst.homeUrl()
