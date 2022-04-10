5edaf9dc2fe7361ed922fa04ddaa07c2d672ac67
qinkaiyuan <socialuni@163.com>
2022-04-10 14:21:43 +0800

    支持解封用户

--- socialuni-admin-ui/src/constants/GlobalConst.ts
+++ socialuni-admin-ui/src/constants/GlobalConst.ts
@@ -4,6 +4,7 @@ import ForbidStatus from '@/constants/audit/ForbidStatus'
 import CosImgResultType from '@/constants/audit/CosImgResultType'
 import ViolateType from '@/constants/ViolateType'
 import UserStatus from '@/constants/UserStatus'
+import DataTypeStrEnum from '@/constants/enum/DataTypeStrEnum'
 
 export default class GlobalConst {
     static readonly OperationType: typeof OperationType = OperationType
@@ -12,4 +13,5 @@ export default class GlobalConst {
     static readonly CosImgResultType: typeof CosImgResultType = CosImgResultType
     static readonly ViolateType: typeof ViolateType = ViolateType
     static readonly UserStatus: typeof UserStatus = UserStatus
+    static readonly DataTypeStrEnum: typeof DataTypeStrEnum = DataTypeStrEnum
 }
