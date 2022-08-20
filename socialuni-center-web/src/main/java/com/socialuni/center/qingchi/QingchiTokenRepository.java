17b2041dddbc6b73829f409087dab88988fa0c52
socialuni <socialuni@163.com>
2022-08-20 13:08:04 +0800

    移除个体应用

--- socialuni-center-web/src/main/java/com/socialuni/center/qingchi/QingchiTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.socialuni.center.qingchi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QingchiTokenRepository extends JpaRepository<QingchiTokenDO, Integer> {
-    QingchiTokenDO findOneByToken(String token);
-}
