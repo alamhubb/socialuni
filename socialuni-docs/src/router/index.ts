dbe85e3949af86de0c333488eb14c63a955f7607
socialuni <socialuni@163.com>
2022-09-01 02:25:41 +0800

    首页整理

--- socialuni-docs/src/router/index.ts
+++ socialuni-docs/src/router/index.ts
@@ -2,6 +2,8 @@ import type {RouteRecordRaw} from "vue-router";
 import {createRouter, createWebHistory} from 'vue-router'
 import Layout from "@/layout/Layout.vue";
 import HomeView from '@/views/home/HomeView.vue'
+import Introduction from '@/views/guide/introduction/introduction.md'
+import MoreUsage from '@/views/guide/moreUsage/moreUsage.md'
 
 export const constantRouters: RouteRecordRaw[] = [
     {
@@ -27,13 +29,13 @@ export const constantRouters: RouteRecordRaw[] = [
                 path: 'introduction',
                 name: 'introduction',
                 meta: {title: '入门'},
-                component: import('@/views/guide/introduction/introduction.md')
+                component: Introduction
             },
             {
                 path: 'moreUsage',
                 name: 'moreUsage',
                 meta: {title: '更多用法'},
-                component: import('@/views/guide/moreUsage/moreUsage.md')
+                component: MoreUsage
             }
         ]
 
