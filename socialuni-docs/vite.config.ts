e8e8984f28e8f9e2aa79531fee35638ffb0fb558
socialuni <socialuni@163.com>
2022-08-31 20:06:19 +0800

    继续使用scss

--- socialuni-docs/vite.config.ts
+++ socialuni-docs/vite.config.ts
@@ -1,22 +1,22 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import markdown from 'vite-plugin-md'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue({
-            include: [/\.vue$/, /\.md$/]
-        }),
-        markdown()
-    ],
-    server: {
-        host: "0.0.0.0"
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    markdown()
+  ],
+  server: {
+    host: "0.0.0.0"
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
 })
