424f2032582e400d636b2097d95337666da4821f
socialuni <socialuni@163.com>
2022-08-31 15:21:03 +0800

    文档编写

--- socialuni-docs/src/views/home/HomeView.vue
+++ socialuni-docs/src/views/home/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="p-8 bg-red-50">123</div>
-    <markdown :src="import('/src/views/home/home.md')">
-    </markdown>
+<!--    <markdown :src="import('/src/views/home/home.md')">
+    </markdown>-->
   </div>
 </template>
 <script setup lang="ts">
--- socialuni-docs/vite.config.ts
+++ socialuni-docs/vite.config.ts
@@ -3,13 +3,21 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import markdown from 'vite-plugin-md'
+// vite.config.ts
+import Unocss from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/]
     }),
-    markdown()
+    markdown(),
+    // Unocss({
+    //   presets: [], // disable default preset
+    //   rules: [
+    //     // your custom rules
+    //   ],
+    // }),
   ],
   server: {
     host: "0.0.0.0"
