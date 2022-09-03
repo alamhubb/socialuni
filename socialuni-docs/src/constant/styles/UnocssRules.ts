1bb250badb469e55b9434b699454828865a6c778
socialuni <socialuni@163.com>
2022-09-03 12:08:10 +0800

    正确支持样式

--- socialuni-docs/src/constant/styles/UnocssRules.ts
+++ socialuni-docs/src/constant/styles/UnocssRules.ts
@@ -7,6 +7,8 @@ export default class UnocssRules {
     static rules: Rule[] = [
         [/^p(.)(-(\D+))?$/, match => UnocssRuleUtil.getSizeStyles('padding', match)],
         [/^p(.)-(\d+)(\D+)?$/, match => UnocssRuleUtil.getNumStyles('padding', match)],
+        [/^m(.)(-(\D+))?$/, match => UnocssRuleUtil.getSizeStyles('margin', match)],
+        [/^m(.)-(\d+)(\D+)?$/, match => UnocssRuleUtil.getNumStyles('margin', match)],
         [/^font-?(\d+)(.+)?$/, match => {
             return ({'font-size': `${match[1] || 20}${match[2] || 'px'}`})
         }],
