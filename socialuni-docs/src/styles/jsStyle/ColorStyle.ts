e8d6087a8d8835b507c375bff33b7487239587b7
socialuni <socialuni@163.com>
2022-09-04 12:28:56 +0800

    支持jscss

--- socialuni-docs/src/styles/jsStyle/ColorStyle.ts
+++ socialuni-docs/src/styles/jsStyle/ColorStyle.ts
@@ -1,5 +1,21 @@
+
+
+
+
 export default class ColorStyle {
     static theme = {
         color: '#28a5dc'
+    }
+
+    static blue = {
+        color: '#28a5dc'
+    }
+
+    static color(color: string) {
+
+
+        return {
+            color: color
+        }
     }
 }
--- socialuni-docs/src/styles/jsStyle/FontWeightStyle.ts
+++ socialuni-docs/src/styles/jsStyle/FontWeightStyle.ts
@@ -4,7 +4,4 @@ export default class FontWeightStyle {
     static bold = {
         fontWeight: 'bold'
     }
-    static theme= {
-        color: '#28a5dc'
-    }
 }
