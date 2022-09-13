570090b19684288cf8db17540d155e6c3be6848c
socialuni <socialuni@163.com>
2022-09-13 11:43:04 +0800

    删除无用类

--- socialuni-sdk/src/main/java/com/socialuni/sdk/config/SocialuniAppConfiguration.java
+++ socialuni-sdk/src/main/java/com/socialuni/sdk/config/SocialuniAppConfiguration.java
@@ -1,18 +1,9 @@
 package com.socialuni.sdk.config;
 
-import com.socialuni.sdk.config.SocialRequestUserConfigDefaultImpl;
-import com.socialuni.sdk.config.SocialuniAppConfigurer;
-import com.socialuni.sdk.constant.TalkTabType;
 import com.socialuni.sdk.logic.domain.talk.SocialuniTalkQueryGenerateQueryBOByTabDomain;
-import com.socialuni.social.web.sdk.config.SocialRequestUserConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-
-import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.List;
 
 @Configurable
 public class SocialuniAppConfiguration {
