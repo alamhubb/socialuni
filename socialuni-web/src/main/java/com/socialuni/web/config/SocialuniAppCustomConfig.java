e543bea49754478574d6b371b2422274cc002a7e
socialuni <socialuni@163.com>
2022-09-13 10:16:38 +0800

    支持自定义类型

--- socialuni-web/src/main/java/com/socialuni/web/config/SocialuniAppCustomConfig.java
+++ socialuni-web/src/main/java/com/socialuni/web/config/SocialuniAppCustomConfig.java
@@ -1,12 +1,24 @@
 package com.socialuni.web.config;
 
 import com.socialuni.sdk.config.SocialuniAppConfig;
+import com.socialuni.sdk.config.SocialuniAppConfigBO;
 import com.socialuni.sdk.constant.config.SocialuniAppType;
+import com.socialuni.sdk.constant.socialuni.GenderType;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
 public class SocialuniAppCustomConfig extends SocialuniAppConfig {
     public static String appType = SocialuniAppType.school;
+
+    //首页的tab栏目
+    public static SocialuniAppConfigBO appConfig;
+
+    //可以通过这里给appConfig自定义，可参考父类的方式
+    static {
+
+    }
 }
