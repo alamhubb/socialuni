4e2843674b2fb647df69326e2d6e6db8bc275e49
socialuni <socialuni@163.com>
2022-09-13 14:14:54 +0800

    修复缺少手机绑定相关代码

--- socialuni-sdk/src/main/java/com/socialuni/sdk/logic/service/phone/SocialuniPhoneService.java
+++ socialuni-sdk/src/main/java/com/socialuni/sdk/logic/service/phone/SocialuniPhoneService.java
@@ -2,10 +2,13 @@ package com.socialuni.sdk.logic.service.phone;
 
 import com.socialuni.sdk.constant.AuthType;
 import com.socialuni.sdk.logic.domain.phone.SocailSendAuthCodeDomain;
+import com.socialuni.sdk.logic.domain.phone.SocialBindPhoneNumDomain;
+import com.socialuni.sdk.logic.domain.phone.SocialBindWxPhoneNumDomain;
 import com.socialuni.sdk.logic.domain.thirdUser.AuthThirdUserDomain;
 import com.socialuni.sdk.logic.entity.user.SocialPhoneLoginEntity;
 import com.socialuni.sdk.dao.DO.dev.DevAccountDO;
 import com.socialuni.sdk.dao.DO.user.SocialuniUserDO;
+import com.socialuni.sdk.model.QO.SocialBindWxPhoneNumQO;
 import com.socialuni.sdk.model.QO.user.SocialPhoneNumQO;
 import com.socialuni.sdk.model.RO.user.SocialuniMineUserDetailRO;
 import com.socialuni.sdk.model.RO.user.login.SocialLoginRO;
@@ -28,6 +31,10 @@ public class SocialuniPhoneService {
     private SocialPhoneLoginEntity socialPhoneLoginEntity;
     @Resource
     private AuthThirdUserDomain authThirdUserDomain;
+    @Resource
+    SocialBindWxPhoneNumDomain socialBindWxPhoneNumDomain;
+    @Resource
+    SocialBindPhoneNumDomain socialBindPhoneNumDomain;
 
     @Transactional
     public ResultRO<SocialLoginRO<SocialuniMineUserDetailRO>> phoneLogin(SocialPhoneNumQO socialPhoneNumQO) {
@@ -44,5 +51,21 @@ public class SocialuniPhoneService {
         SocialuniUserDO mineUser = SocialuniUserUtil.getMineUserAllowNull();
         //校验逻辑应该拿到 domain里，因为限制了只有清池可以访问，所以不再限制ip
         return socailSendAuthCodeDomain.sendAuthCode(authCodeQO, mineUser);
+    }
+
+    public ResultRO<SocialuniMineUserDetailRO> bindPhoneNum(SocialPhoneNumQO socialPhoneNumQO) {
+        SocialuniUserDO mineUser = SocialuniUserUtil.getMineUserAllowNull();
+
+        SocialuniMineUserDetailRO socialMineUserDetailRO = socialBindPhoneNumDomain.bindPhoneNum(socialPhoneNumQO, mineUser);
+
+        return new ResultRO<>(socialMineUserDetailRO);
+    }
+
+    public ResultRO<SocialuniMineUserDetailRO> bindWxPhoneNum(SocialBindWxPhoneNumQO socialBindWxPhoneNumQO) {
+        SocialuniUserDO mineUser = SocialuniUserUtil.getMineUserAllowNull();
+        //微信绑定手机号方法
+        SocialuniMineUserDetailRO socialMineUserDetailRO = socialBindWxPhoneNumDomain.bindWxPhoneNum(socialBindWxPhoneNumQO, mineUser);
+
+        return new ResultRO<>(socialMineUserDetailRO);
     }
 }
--- socialuni-web/src/main/java/com/socialuni/web/controller/SocialuniPhoneController.java
+++ socialuni-web/src/main/java/com/socialuni/web/controller/SocialuniPhoneController.java
@@ -1,29 +1,43 @@
 package com.socialuni.web.controller;
 
 import com.socialuni.sdk.logic.service.phone.SocialuniPhoneService;
+import com.socialuni.sdk.model.QO.SocialBindWxPhoneNumQO;
 import com.socialuni.sdk.model.QO.user.SocialPhoneNumQO;
 import com.socialuni.sdk.model.RO.user.SocialuniMineUserDetailRO;
 import com.socialuni.social.web.sdk.model.ResultRO;
 import com.socialuni.sdk.model.RO.user.login.SocialLoginRO;
 import com.socialuni.sdk.model.RO.user.phone.SocialSendAuthCodeQO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("phone")
 public class SocialuniPhoneController implements SocialuniPhoneAPI {
     @Resource
-    SocialuniPhoneService centerPhoneService;
+    SocialuniPhoneService socialuniPhoneService;
 
     @Override
     public ResultRO<Void> sendAuthCode(SocialSendAuthCodeQO authCodeQO) {
-        return centerPhoneService.sendAuthCode(authCodeQO);
+        return socialuniPhoneService.sendAuthCode(authCodeQO);
     }
 
     @Override
     public ResultRO<SocialLoginRO<SocialuniMineUserDetailRO>> phoneLogin(SocialPhoneNumQO socialPhoneNumQO) {
-        return centerPhoneService.phoneLogin(socialPhoneNumQO);
+        return socialuniPhoneService.phoneLogin(socialPhoneNumQO);
+    }
+
+    @PostMapping("bindWxPhoneNum")
+    public ResultRO<SocialuniMineUserDetailRO> bindWxPhoneNum(@RequestBody @Valid SocialBindWxPhoneNumQO bindWxPhoneNumQO) {
+        return socialuniPhoneService.bindWxPhoneNum(bindWxPhoneNumQO);
+    }
+
+    @PostMapping("bindPhoneNum")
+    public ResultRO<SocialuniMineUserDetailRO> bindPhoneNum(@RequestBody @Valid SocialPhoneNumQO phoneNumQO) {
+        return socialuniPhoneService.bindPhoneNum(phoneNumQO);
     }
 }
