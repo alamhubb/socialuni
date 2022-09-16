b5a2d7e20cb7610a7feabdba9e36265dbe5435e0
socialuni <socialuni@163.com>
2022-09-16 17:40:19 +0800

    修改默认配置

--- socialuni-sdk/src/main/java/com/socialuni/sdk/config/SocialuniAppConfigBO.java
+++ socialuni-sdk/src/main/java/com/socialuni/sdk/config/SocialuniAppConfigBO.java
@@ -20,9 +20,4 @@ public class SocialuniAppConfigBO {
     private Boolean hasSchoolCanPostTalk = false;
     //配置3，应用性别
     private String appGender = GenderType.all;
-
-    //系统异常错误消息
-//    private String errorMsg601UnLogin = "未登录，请进行登录";
-//    private String errorMsg604SystemError = "系统异常，有任何疑问请联系客服";
-//    private String errorMsg605UserBaned = "账号违规，被封禁，有任何疑问请联系客服";
 }
