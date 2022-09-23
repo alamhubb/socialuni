71f20d79aecb52a32a104401f73e3eacefeb747e
wlh <873406903@qq.com>
2022-09-23 22:56:13 +0800

    包名没扫到。

--- socialuni-web/src/main/java/com/socialuni/web/SocialuniWebApplication.java
+++ socialuni-web/src/main/java/com/socialuni/web/SocialuniWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableSocialuni
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.socialuni.web","com.socialuni"})
 //@EnableCaching
 public class SocialuniWebApplication {
     public static void main(String[] args) {
