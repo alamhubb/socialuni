a9fa2f45006b079d8cb0c7d904351822fd8638d1
socialuni <socialuni@163.com>
2022-09-26 17:25:04 +0800

    统一报名，升级版本

--- social-web-sdk/src/main/java/com/socialuni/social/web/sdk/repository/ErrorRequestLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.socialuni.social.web.sdk.repository;
-
-
-import com.socialuni.social.web.sdk.model.ErrorRequestLogDO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ErrorRequestLogRepository extends JpaRepository<ErrorRequestLogDO, Long> {
-}
-
--- social-web-sdk/src/main/java/com/socialuni/social/web/sdk/repository/RequestLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.socialuni.social.web.sdk.repository;
-
-
-import com.socialuni.social.web.sdk.model.RequestLogDO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestLogRepository extends JpaRepository<RequestLogDO, Long> {
-}
-
