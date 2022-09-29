febf487d5ed3cfa3e96e8298760d9aafad160c7b
wlh <873406903@qq.com>
2022-09-29 16:08:53 +0800

    用线程+拦截器。 代替Scope 'request' 的使用。

--- socialuni-web-sdk/src/main/java/com/socialuni/social/web/sdk/config/SocialuniWebInterceptorConfiguration.java
+++ socialuni-web-sdk/src/main/java/com/socialuni/social/web/sdk/config/SocialuniWebInterceptorConfiguration.java
@@ -3,11 +3,13 @@ package com.socialuni.social.web.sdk.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class SocialuniWebInterceptorConfiguration {
 
     @Bean
+    @Primary
     @ConditionalOnMissingBean(SocialuniWebInterceptor.class)
     public SocialuniWebInterceptor defaultAuthInterceptor() {
         return new SocialuniWebInterceptor();
