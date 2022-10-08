c06f8c7f11d3765e8bd6e20878bd46613c3a4c2e
wlh <873406903@qq.com>
2022-10-08 18:42:38 +0800

    spring-boot 2.7版本跨域的其他配置。

--- socialuni-common/src/main/java/com/socialuni/social/common/config/SocialuniWebMvcConfigurer.java
+++ socialuni-common/src/main/java/com/socialuni/social/common/config/SocialuniWebMvcConfigurer.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,8 +22,27 @@ public class SocialuniWebMvcConfigurer implements WebMvcConfigurer {
     @Autowired(required = false)
     @Qualifier("tanceHandlerInterceptor")
     private HandlerInterceptor tanceHandlerInterceptor;
-
-    @Override
+    @Bean
+    public CorsFilter corsFilter() {
+        //1. 添加 CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //放行哪些原始域
+        config.addAllowedOriginPattern("*");
+        //是否发送 Cookie
+        config.setAllowCredentials(true);
+        //放行哪些请求方式
+        config.addAllowedMethod("*");
+        //放行哪些原始请求头部信息
+        config.addAllowedHeader("*");
+        //暴露哪些头部信息
+//        config.addExposedHeader("*");
+        //2. 添加映射路径
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**",config);
+        //3. 返回新的CorsFilter
+        return new CorsFilter(corsConfigurationSource);
+    }
+/*    @Override
     public void addCorsMappings(CorsRegistry registry) {
         //设置允许跨域的路径
         registry.addMapping("/**")
@@ -36,7 +57,7 @@ public class SocialuniWebMvcConfigurer implements WebMvcConfigurer {
 //                .exposedHeaders(HttpHeaders.SET_COOKIE, "haha")
                 //跨域允许时间
                 .maxAge(3600);
-    }
+    }*/
 
     @Bean
     public RestTemplate restTemplate() {
