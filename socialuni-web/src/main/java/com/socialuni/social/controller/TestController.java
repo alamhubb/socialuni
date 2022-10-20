481bb22fdc23a100ff8097ff89721b4dcfee7a11
wlh <873406903@qq.com>
2022-10-20 15:13:25 +0800

    支持注释写文档啦。

--- socialuni-web/src/main/java/com/socialuni/social/controller/TestController.java
+++ socialuni-web/src/main/java/com/socialuni/social/controller/TestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    /**
+     * 测试的请求。
+     * @return str 测试内容
+     */
     @RequestMapping("hello")
     public String hello(){
         return "st";
