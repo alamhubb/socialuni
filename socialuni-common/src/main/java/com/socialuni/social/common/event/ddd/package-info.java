207ff9ea1873335dd77045839738a696e9f3dab2
wlh <873406903@qq.com>
2022-10-20 18:15:52 +0800

    添加ddd的事件抽象内容

---
+++ socialuni-common/src/main/java/com/socialuni/social/common/event/ddd/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 为了抽象ddd的模型领域内容。
+ * 应该和spring的事件做隔离。
+ * 这里起到一个技术层的适配和防腐层。
+ * @module Socialuni
+ * @author wulinghui
+ * @date 2022/10/20 16:58
+ * @version 1.0
+ * @since 1.0
+ */
+package com.socialuni.social.common.event.ddd;
