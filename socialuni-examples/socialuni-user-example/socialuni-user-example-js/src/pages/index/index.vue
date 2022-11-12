e170f3b61a713cb7ef650022a10e128f289b3759
socialuni <socialuni@163.com>
2022-11-12 21:05:48 +0800

    aaa

---
+++ socialuni-examples/socialuni-user-example/socialuni-user-example-js/src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title" @click="haha">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-property-decorator'
+
+@Options({
+})
+export default class IndexPage extends Vue {
+  title = 'asdf'
+
+  haha(){
+    console.log(123)
+    console.log(333)
+    console.log(4444)
+    console.log(5555)
+  }
+}
+</script>
