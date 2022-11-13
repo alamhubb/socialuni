0d1504a8dbab682bbf8ba257d97939adaecba5b8
socialuni <socialuni@163.com>
2022-11-14 00:56:29 +0800

    支持显示chat模块

---
+++ socialuni-examples/socialuni-im-example/socialuni-im-example-js/src/pagesLazy/chat/message.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="bg-default h100p">
+    <message-view class="h100p"></message-view>
+  </view>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-property-decorator'
+import ChatView from "socialuni-im/src/views/chat/ChatView.vue";
+import MessageView from "socialuni-im/src/views/message/MessageView.vue";
+
+@Options({
+  components:{
+    ChatView,
+    MessageView
+  }
+})
+export default class MessagePage extends Vue {
+
+}
+</script>
