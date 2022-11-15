8f7b3e22c01c13268bccb4f7b7255de3ee516933
wlh <873406903@qq.com>
2022-11-15 18:57:25 +0800

    脚本自己修改完成。

--- socialuni-server/socialuni-common-sdk/src/main/java/com/socialuni/social/common/sdk/tools/RedisLimitTools.java
+++ socialuni-server/socialuni-common-sdk/src/main/java/com/socialuni/social/common/sdk/tools/RedisLimitTools.java
@@ -2,6 +2,7 @@ package com.socialuni.social.common.sdk.tools;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.scripting.support.ResourceScriptSource;
@@ -25,41 +26,27 @@ public class RedisLimitTools {
 
     private StringRedisTemplate redisTemplate;
 
-    public void test() {
-
+    public void test(String key) {
         //调用lua脚本并执行
         DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
         redisScript.setResultType(Boolean.class);//返回类型是Long
-        redisScript.setScriptText("local sms_send_key = KEYS[1];\n" +
-                "local sms_send_value = ARGV[1];\n" +
+        redisScript.setScriptText(
+                "local sms_send_key = KEYS[1];\n" +
+                "local sms_send_limit = ARGV[1];\n" +
                 "local expire_time = ARGV[2];\n" +
-                "--解析string字符串 分解出.前缀和.后缀(str = '1.234'  prefix = 1 suffix = 234)\n" +
-                "local function parse(str)\n" +
-                "    local start_index ,end_index = string.find(str,\".\",1)\n" +
-                "    local prefix = string.sub(str,1,start_index)\n" +
-                "    local suffix = string.sub(str,start_index+2,-1)\n" +
-                "    return prefix,suffix\n" +
-                "end\n" +
-                " \n" +
-                "local value  = redis.call(\"setnx\",sms_send_key,sms_send_value)\n" +
-                "--设置成功 说明之前不存在\n" +
-                "if(value == 1)\n" +
-                "      then   redis.call(\"expire\",sms_send_key,expire_time)\n" +
-                "      return true;\n" +
-                "else\n" +
-                "      local oldValue = tostring(redis.call(\"get\",sms_send_key))\n" +
-                "      local oldPre,oldSuf = parse(oldValue)\n" +
-                "      local newPre,newSuf = parse(sms_send_value)\n" +
-                "--超过60分钟可以发送\n" +
-                "        if tonumber(oldPre) <=2 and (tonumber(newSuf) - tonumber(oldSuf)) >= 60\n" +
-                "            then\n" +
-                "            local newValue = tostring(-0)..'.'..tostring(oldSuf)\n" +
-                "            redis.call(\"incrbyfloat\",sms_send_key,sms_send_value)\n" +
-                "            redis.call(\"incrbyfloat\",sms_send_key,tonumber(newValue))\n" +
-                "            return true;\n" +
-                "        else\n" +
-                "            return false;\n" +
-                "        end\n" +
+                "local oldValue = redis.call(\"get\",sms_send_key);\n" +
+                "if (oldValue == nil) then\n" +
+                "\t--- 之前不存在\n" +
+                "\tredis.call(\"incr\",sms_send_key);\n" +
+                "\tredis.call(\"expire\",sms_send_key,expire_time);\n" +
+                "\treturn true;\n" +
+                "elseif  ( tonumber(oldValue) > sms_send_limit )  then\n" +
+                "\t--- 自增\n" +
+                "\tredis.call(\"incr\",sms_send_key);\n" +
+                "\treturn true;\n" +
+                "else \n" +
+                "\t--- 失败，不操作。\n" +
+                "\treturn false;\n" +
                 "end");
         LocalDateTime now = LocalDateTime.now();
         int dayOfYear = now.getDayOfYear();
