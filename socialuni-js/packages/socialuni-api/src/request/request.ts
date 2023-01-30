bc8969d4b4f98e107e51bc8aa546a60a375d82bb
wlh <873406903@qq.com>
2023-01-30 11:58:38 +0800

    解决发布wx小程序，连接不上的问题。  升级sdk版本。

--- socialuni-js/packages/socialuni-api/src/request/request.ts
+++ socialuni-js/packages/socialuni-api/src/request/request.ts
@@ -30,7 +30,7 @@ request.interceptor.request((config: requestConfig) => { /* 请求之前拦截�
         config.header.token = null
     }
     // 如果配置了开发环境，就可以展示具体的报错内容。
-    config.header['X-NODE-ENV'] = process?.env?.NODE_ENV
+    config.header['X-NODE-ENV'] = process.env.NODE_ENV
     //
     config.header.socialuniSecretKey = "f12be0da13bf4cf9b672c5e7957c71f2"
     config.header.provider = socialSystemModule.mpPlatform
