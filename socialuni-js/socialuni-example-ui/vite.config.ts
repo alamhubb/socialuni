503b3f2d4c4555ffa41634b4640d77171628f323
socialuni <socialuni@163.com>
2023-02-15 13:46:38 +0800

    修复h5环境问题

--- socialuni-js/socialuni-example-ui/vite.config.ts
+++ socialuni-js/socialuni-example-ui/vite.config.ts
@@ -3,9 +3,41 @@ import uni from '@dcloudio/vite-plugin-uni'
 import requireTransform from 'vite-plugin-require-transform';
 import commonjs from "@rollup/plugin-commonjs";
 import {fileURLToPath, URL} from "node:url";
+
 const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
-
+let plugins = []
+if (IN_PRODUCTION) {
+    plugins = [
+        require('@fullhuman/postcss-purgecss')({
+            // require('@fullhuman/postcss-purgecss')({
+            content: ['./public/**/*.html', './node_modules/socialuni-view/**/*.scss', './node_modules/@dcloudio/**/*.scss', './node_modules/@dcloudio/**/*.vue', './node_modules/socialuni-view/**/*.vue', './src/**/*.vue'],
+            defaultExtractor(content) {
+                const contentWithoutStyleBlocks = content.replace(
+                    /<style[^]+?<\/style>/gi,
+                    ''
+                )
+                return (
+                    contentWithoutStyleBlocks.match(
+                        /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+                    ) || []
+                )
+            },
+            safelist: [
+                /-(leave|enter|appear)(|-(to|from|active))$/,
+                /^(?!(|.*?:)cursor-move).+-move$/,
+                /^router-link(|-exact)-active$/,
+                /data-v-.*/,
+                /uicon-.*/,
+                /q-.*/,
+                /u-.*/,
+                /uni-.*/,
+                /mdi-.*/,
+                /page/
+            ]
+        })
+    ]
+}
 // vite.config.ts
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,37 +52,9 @@ export default defineConfig({
     server: {
         host: "0.0.0.0"
     },
-    css:{
-        postcss:{
-            plugins:[
-                IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
-                    // require('@fullhuman/postcss-purgecss')({
-                    content: ['./public/**/*.html','./node_modules/socialuni-view/**/*.scss','./node_modules/@dcloudio/**/*.scss','./node_modules/@dcloudio/**/*.vue','./node_modules/socialuni-view/**/*.vue', './src/**/*.vue'],
-                    defaultExtractor (content) {
-                        const contentWithoutStyleBlocks = content.replace(
-                            /<style[^]+?<\/style>/gi,
-                            ''
-                        )
-                        return (
-                            contentWithoutStyleBlocks.match(
-                                /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-                            ) || []
-                        )
-                    },
-                    safelist: [
-                        /-(leave|enter|appear)(|-(to|from|active))$/,
-                        /^(?!(|.*?:)cursor-move).+-move$/,
-                        /^router-link(|-exact)-active$/,
-                        /data-v-.*/,
-                        /uicon-.*/,
-                        /q-.*/,
-                        /u-.*/,
-                        /uni-.*/,
-                        /mdi-.*/,
-                        /page/
-                    ]
-                })
-            ]
+    css: {
+        postcss: {
+            plugins: plugins
         }
     },
     resolve: {
