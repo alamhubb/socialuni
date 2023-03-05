aeea458e8ecbe3ef46c3893283cbee2ce868158d
qinkaiyuan <qinkaiyuan94@163.com>
2023-03-05 10:10:46 +0800

    修复 mac 无法启动问题

--- socialuni-js/socialuni-example-ui/vite.config.ts
+++ socialuni-js/socialuni-example-ui/vite.config.ts
@@ -60,7 +60,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
-            'stream': 'stream-browserify'
         }
     }
 })
