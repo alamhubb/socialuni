eb0de20270400d2ee4dbd00819e7f4bf01a406c1
socialuni <socialuni@163.com>
2023-03-08 17:07:10 +0800

    修复tag问题，提取tag组件

--- socialuni-js/packages/socialuni-view/src/views/user/coin/SocialuniCoinView.vue
+++ socialuni-js/packages/socialuni-view/src/views/user/coin/SocialuniCoinView.vue
@@ -2,17 +2,17 @@
   <view class="bg-blue6 h100p flex-col">
     <div class="chunk-blue6 px-smm py-md col-all-center flex-none">
       <div class="font-12">贝壳总数</div>
-      <div class="font-bold font-28 mt"> {{ user.socialCoin }}</div>
+      <div class="font-bold font-28 mt"> {{ mineUser.socialCoin }}</div>
     </div>
 
     <div class="bg-white bt-radius-30 flex-1 pb-smm px-smm">
-<!--      <div class="flex-row">
-        <div v-for="type in coinPageTypes"
-             class="flex-1 row-all-center chunk-theme_light q-box color-content mx-xs use-click"
-             @click="toCoinRecordPage(type)">{{ type }}记录
-        </div>
-      </div>-->
-<!--      <div class="bt mt pt">-->
+      <!--      <div class="flex-row">
+              <div v-for="type in coinPageTypes"
+                   class="flex-1 row-all-center chunk-theme_light q-box color-content mx-xs use-click"
+                   @click="toCoinRecordPage(type)">{{ type }}记录
+              </div>
+            </div>-->
+      <!--      <div class="bt mt pt">-->
       <div class="mt pt">
         <text class="row-all-center font-lg font-bold">
           充值
@@ -36,7 +36,7 @@
         </div>
 
         <div class="mt">
-          <q-button class="w100p font-bold" @click="userPay" md theme>充值</q-button>
+          <q-button class="w100p font-bold" @click="userPay" md theme :disabled="disabled">充值</q-button>
         </div>
       </div>
     </div>
@@ -70,7 +70,9 @@ export default class SocialuniCoinView extends Vue {
 
   checkedPayValue = 1
 
-  get user() {
+  disabled = false
+
+  get mineUser() {
     return socialUserModule.mineUser
   }
 
@@ -84,10 +86,16 @@ export default class SocialuniCoinView extends Vue {
     })
   }
 
-  userPay() {
+  async userPay() {
+    this.disabled = true
     //如果是app则默认微信支付，否则就是当前环境,app 微信，其他微信mp微信，qqmp，qq
     // const provider = socialSystemModule.isMp ? socialSystemModule.provider : SocialuniProviderType.wx
-    PlatformUtils.payCoin(this.checkedPayValue)
+    try {
+      await PlatformUtils.payCoin(this.checkedPayValue)
+      this.mineUser.socialCoin += this.checkedPayValue * 100
+    } finally {
+      this.disabled = false
+    }
   }
 
   toCoinRecordPage(pageType: string) {
