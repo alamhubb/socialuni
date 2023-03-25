c698846295e7e201fc936aa50d2c10d6d959dc4a
socialuni <socialuni@163.com>
2023-03-25 13:39:14 +0800

    支持添加好友功能

--- socialuni-server/socialuni-im/src/main/java/com/socialuni/social/im/logic/service/SocialuniFriendService.java
+++ socialuni-server/socialuni-im/src/main/java/com/socialuni/social/im/logic/service/SocialuniFriendService.java
@@ -1,6 +1,8 @@
 package com.socialuni.social.im.logic.service;
 
 
+import com.socialuni.social.common.api.entity.SocialuniUserContactBaseDO;
+import com.socialuni.social.common.api.enumeration.SocialuniCommonStatus;
 import com.socialuni.social.common.api.exception.exception.SocialBusinessException;
 import com.socialuni.social.common.sdk.dao.facede.SocialuniUserContactRepositoryFacede;
 import com.socialuni.social.im.dao.DO.SocialuniFriendApplyRecordDO;
@@ -17,15 +19,16 @@ public class SocialuniFriendService {
         Integer mineUserId = SocialuniUserUtil.getMineUserIdNotNull();
         //添加对方的时候只看对方的状态，不看自己的状态
 
-        //判断是否已为好友
-        SocialuniFriendApplyRecordDO socialuniFriendDO = SocialuniUserContactRepositoryFacede.findByUserIdAndBeUserIdAndStatus(mineUserId, beUserId, SocialuniAddFriendStatus.enable, SocialuniFriendApplyRecordDO.class);
+        //判断是否已为好友, 看你是不是对方的好有
+        SocialuniFriendApplyRecordDO socialuniFriendDO = SocialuniUserContactRepositoryFacede.findByUserIdAndBeUserIdAndStatus(beUserId, mineUserId, SocialuniAddFriendStatus.enable, SocialuniFriendApplyRecordDO.class);
         if (socialuniFriendDO != null) {
             throw new SocialBusinessException("对方已经是您的好友了，禁止重复添加");
         }
 
-        //对方删除和未响应的数据
+        //对方未响应的数据
         Long countNum = SocialuniUserContactRepositoryFacede.countByUserIdAndBeUserIdAndStatus(mineUserId, beUserId, SocialuniAddFriendStatus.init, SocialuniFriendApplyRecordDO.class);
 
+        //禁止多次添加，如果2次以上对上不加不能再申请
         if (countNum > 2) {
             throw new SocialBusinessException("禁止频繁添加同一人好友");
         }
@@ -34,19 +37,25 @@ public class SocialuniFriendService {
         //首先生成一条记录
         SocialuniFriendApplyRecordDO socialuniFriendApplyRecordDO = new SocialuniFriendApplyRecordDO(mineUserId, beUserId);
 
+        //发起申请成功
         socialuniFriendApplyRecordDO = SocialuniUserContactRepositoryFacede.save(socialuniFriendApplyRecordDO);
 
 
         //如果对方有初始，加过你，或者正常，你把对方删了的情况，则成功为好友
-        //判断对方是否已添加过你，忽略也为初始，没有拒绝状态
-        Long beUserAddUserCount = SocialuniUserContactRepositoryFacede.countByUserIdAndBeUserIdAndNotStatus(beUserId, mineUserId, SocialuniAddFriendStatus.delete, SocialuniFriendApplyRecordDO.class);
+        //有对方不为把你删除的记录
+        SocialuniFriendApplyRecordDO beUserFriendApplyRecordDO = SocialuniUserContactRepositoryFacede.countByUserIdAndBeUserIdAndNotStatus(beUserId, mineUserId, SocialuniAddFriendStatus.delete, SocialuniFriendApplyRecordDO.class);
 
-        //删除好友只会把自己这边的状态改为删除
-        if (beUserAddUserCount > 0) {
-            socialuniFriendApplyRecordDO.setStatus(SocialuniAddFriendStatus.enable);
-
-
+        if (beUserFriendApplyRecordDO == null) {
+            return;
         }
+        //添加好友成功
+        //删除好友只会把自己这边的状态改为删除
+        socialuniFriendApplyRecordDO.setStatus(SocialuniAddFriendStatus.enable);
+        SocialuniUserContactRepositoryFacede.save(socialuniFriendApplyRecordDO);
+
+        beUserFriendApplyRecordDO.setStatus(SocialuniAddFriendStatus.enable);
+        SocialuniUserContactRepositoryFacede.save(beUserFriendApplyRecordDO);
+
     }
 
     //删除朋友
