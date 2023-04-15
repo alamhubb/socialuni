8f0b83726657ea8f1e5d373778246874e1aa7b35
socialuni <socialuni@163.com>
2023-04-15 20:27:03 +0800

    分包

---
+++ socialuni-js/socialuni-test-ui/src/pages/chat/chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <chat-view></chat-view>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-property-decorator'
+import ChatView from 'socialuni-im/src/view/chat/ChatView.vue'
+
+@Options({
+  components: {ChatView}
+})
+export default class ChatPage extends Vue {
+
+}
+</script>
