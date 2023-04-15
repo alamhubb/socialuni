e2a962d4a7e7a3d23ca746b77dac981b516904c2
socialuni <socialuni@163.com>
2023-04-15 22:33:17 +0800

    支持插件功能

--- socialuni-js/packages/socialuni/src/index.ts
+++ socialuni-js/packages/socialuni/src/index.ts
@@ -3,24 +3,21 @@ import {onHide, onLoad, onShow} from "@dcloudio/uni-app";
 import UniUtil from "socialuni-util/src/util/UniUtil";
 import {SocialuniPlugin} from "./interface/SocialuniPlugin";
 import {ImportModule} from "./interface/ImportModule";
+import {socialuniPluginsModule} from "./store/SocialuniPluginsModule";
 
 
-async function installSocialuniPluginIns(){
+async function installSocialuniPluginIns() {
     try {
         //查询是否包含community模块，如果存在则加载
         const socialuniCommunityPlugin: ImportModule<SocialuniPlugin> = await import('socialuni-community/src/index');
-        if (socialuniCommunityPlugin) {
-            socialuniCommunityPlugin.default.onLaunch()
-        }
+        socialuniPluginsModule.addPlugin(socialuniCommunityPlugin.default)
     } catch (e) {
         // 如果导入失败，则不触发任何操作
     }// 社交联盟内置支持的插件
     try {
         //查询是否包含community模块，如果存在则加载
         const socialuniImPlugin: ImportModule<SocialuniPlugin> = await import('socialuni-im/src/index');
-        if (socialuniImPlugin) {
-            socialuniImPlugin.default.onLaunch()
-        }
+        socialuniPluginsModule.addPlugin(socialuniImPlugin.default)
     } catch (e) {
         // 如果导入失败，则不触发任何操作
     }
@@ -32,10 +29,13 @@ const Socialuni = {
         // 社交联盟内置支持的插件
         await installSocialuniPluginIns()
         if (socialuniPlugins && Array.isArray(socialuniPlugins) && socialuniPlugins.length) {
-            for (const plugin of socialuniPlugins) {
-                plugin.onLaunch()
-            }
+            socialuniPluginsModule.addPlugin(...socialuniPlugins)
         }
+
+        for (const plugin of socialuniPluginsModule.plugins) {
+            plugin.onLaunch()
+        }
+
         const shareComponent = defineComponent({
             onShareAppMessage() {
                 const title = '年轻人生活分享社区'
---
+++ socialuni-js/packages/socialuni/src/store/SocialuniPluginsModule.ts
@@ -0,0 +1,20 @@
+import {reactive, UnwrapNestedRefs} from "vue";
+import {SocialuniPlugin} from "../interface/SocialuniPlugin";
+
+class SocialuniPluginsModule {
+    private socialuniPlugins: SocialuniPlugin[] = []
+
+    init() {
+        this.socialuniPlugins = []
+    }
+
+    addPlugin(...socialuniPlugins: SocialuniPlugin[]) {
+        this.socialuniPlugins.push(...socialuniPlugins)
+    }
+
+    get plugins() {
+        return this.socialuniPlugins
+    }
+}
+
+export const socialuniPluginsModule: UnwrapNestedRefs<SocialuniPluginsModule> = reactive(new SocialuniPluginsModule())
