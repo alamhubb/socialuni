f509db6d7942d9a10a9aea8dd67691980e81d63e
socialuni <socialuni@163.com>
2023-06-28 17:26:52 +0800

    调整样式

--- socialuni-js/packages/socialuni-im/socialuni-im-view-h5/src/views/SocialuniChatView.vue
+++ socialuni-js/packages/socialuni-im/socialuni-im-view-h5/src/views/SocialuniChatView.vue
@@ -110,7 +110,7 @@
 <script lang="ts">
 import {Options, Vue} from 'vue-property-decorator'
 import SScrollbar from "socialuni-ui-h5/src/components/SScrollbar.vue";
-import SocialuniChatViewServiceH5 from "../logic/SocialuniChatViewServiceH5";
+import SocialuniChatViewServiceH5 from "socialuni-im-h5-sdk/src/logic/SocialuniChatViewServiceH5";
 
 @Options({components: {SScrollbar}})
 export default class SocialuniChatView extends Vue {
