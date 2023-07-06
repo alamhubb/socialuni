c20f339981c4d70604d7cbf6c45a22691f878cbb
socialuni <socialuni@163.com>
2023-07-06 18:56:05 +0800

    同步vite打包代码

--- socialuni-js/socialuni-example-uni/vite.config.ts
+++ socialuni-js/socialuni-example-uni/vite.config.ts
@@ -3,23 +3,29 @@ import uni from '@dcloudio/vite-plugin-uni'
 import requireTransform from 'vite-plugin-require-transform';
 import commonjs from "@rollup/plugin-commonjs";
 import {fileURLToPath, URL} from "node:url";
+import visualizer from 'rollup-plugin-visualizer';
+import analyzer from 'rollup-plugin-analyzer';
 
 const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
 let plugins = []
-    plugins = [
-
-    ]
 // vite.config.ts
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
+    build: {
+        minify: false
+    },
     plugins: [
         uni(),
         commonjs(),
         requireTransform({
             fileRegex: /.js$|.vue$/
         }),
+        /*visualizer({
+            open: true
+        }),
+        analyzer()*/
     ],
     server: {
         host: "0.0.0.0"
