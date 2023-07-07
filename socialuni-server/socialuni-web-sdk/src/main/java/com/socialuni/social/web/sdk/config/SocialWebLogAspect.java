d942b8baa807af70447fed846dd0459ab8eeeab6
wulinghui <873406903@qq.com>
2023-07-07 19:01:18 +0800

    // 日志不报错的问题。

--- socialuni-server/socialuni-web-sdk/src/main/java/com/socialuni/social/web/sdk/config/SocialWebLogAspect.java
+++ socialuni-server/socialuni-web-sdk/src/main/java/com/socialuni/social/web/sdk/config/SocialWebLogAspect.java
@@ -47,35 +47,38 @@ public class SocialWebLogAspect {
         }
         String params = Arrays.toString(joinPoint.getArgs());
         requestLogDO.setParams(params);
-
-        Object result = joinPoint.proceed();
-
-        Date endDate = new Date();
-        long spendTime = endDate.getTime() - requestLogDO.getCreateTime().getTime();
-        requestLogDO.setEndTime(endDate);
-        requestLogDO.setSpendTime(spendTime);
-        StringBuilder sb = new StringBuilder();
-        if (result != null) {
-            sb.append("result is not null");
-            if (result instanceof ResultRO) {
-                ResultRO resultRO = (ResultRO) result;
-                requestLogDO.setErrorCode(resultRO.getCode());
-                requestLogDO.setErrorMsg(resultRO.getErrorMsg());
-                requestLogDO.setSuccess(resultRO.getSuccess());
-                sb.append(", instanceof ResultRO ");
-            } else {
-                requestLogDO.setErrorCode(ErrorCode.SYSTEM_ERROR);
-                requestLogDO.setErrorType(ErrorType.error);
-                requestLogDO.setErrorMsg(RequestErrorMsg.getSystemErrorMsg());
-                requestLogDO.setSuccess(false);
-                requestLogDO.setInnerMsg(RequestErrorMsg.getSystemErrorMsg());
-                requestLogDO.setInnerMsgDetail(result.toString());
-                sb.append(",not of instanceof ResultRO ");
-                ErrorLogUtil.saveAsync(requestLogDO);
+        // 日志不报错的问题。
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } finally {
+            Date endDate = new Date();
+            long spendTime = endDate.getTime() - requestLogDO.getCreateTime().getTime();
+            requestLogDO.setEndTime(endDate);
+            requestLogDO.setSpendTime(spendTime);
+            StringBuilder sb = new StringBuilder();
+            if (result != null) {
+                sb.append("result is not null");
+                if (result instanceof ResultRO) {
+                    ResultRO resultRO = (ResultRO) result;
+                    requestLogDO.setErrorCode(resultRO.getCode());
+                    requestLogDO.setErrorMsg(resultRO.getErrorMsg());
+                    requestLogDO.setSuccess(resultRO.getSuccess());
+                    sb.append(", instanceof ResultRO ");
+                } else {
+                    requestLogDO.setErrorCode(ErrorCode.SYSTEM_ERROR);
+                    requestLogDO.setErrorType(ErrorType.error);
+                    requestLogDO.setErrorMsg(RequestErrorMsg.getSystemErrorMsg());
+                    requestLogDO.setSuccess(false);
+                    requestLogDO.setInnerMsg(RequestErrorMsg.getSystemErrorMsg());
+                    requestLogDO.setInnerMsgDetail(result.toString());
+                    sb.append(",not of instanceof ResultRO ");
+                    ErrorLogUtil.saveAsync(requestLogDO);
+                }
             }
+            log.info(sb.toString()); // 添加日志。
+            RequestLogUtil.saveAsyncAndRemove(requestLogDO);
         }
-        log.info(sb.toString()); // 添加日志。
-        RequestLogUtil.saveAsyncAndRemove(requestLogDO);
         return result;
     }
 
