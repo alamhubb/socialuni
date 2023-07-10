7d6fc8b45156e74337df7ce3f33fceda0c49f021
socialuni <socialuni@163.com>
2023-07-11 01:27:24 +0800

    xiufu秘钥问题

--- socialuni-js/packages/socialuni-api/socialuni-api-base/src/SocialuniAPIConfig.ts
+++ socialuni-js/packages/socialuni-api/socialuni-api-base/src/SocialuniAPIConfig.ts
@@ -5,11 +5,5 @@ export default class SocialuniAPIConfig {
     static readonly socialuniImUrl = (import.meta.env.VITE_APP_SOCIALUNI_IM_URL) || this.socialuniUrl;
     static readonly socialuniWebsocketUrl = (import.meta.env.VITE_APP_SOCIALUNI_WEBSOCKET_URL) || 'wss://api.socialuni.cn';
     static readonly socialuniSecretKey = (import.meta.env.VITE_APP_SOCIALUNI_SECRET_KEY) || null;
-    // static readonly socialuniUrl = 'https://api.socialuni.cn'
-    // static readonly socialuniUserUrl = this.socialuniUrl
-    // static readonly socialuniCommunityUrl = this.socialuniUrl
-    // static readonly socialuniImUrl = this.socialuniUrl
-    // static readonly socialuniWebsocketUrl = 'wss://api.socialuni.cn'
-    // static readonly socialuniSecretKey = null
 }
 
