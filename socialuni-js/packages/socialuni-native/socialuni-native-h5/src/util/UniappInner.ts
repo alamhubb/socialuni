8ff1f3eb2ad6553a55a999fe32992add487786c7
socialuni <socialuni@163.com>
2023-07-11 01:20:21 +0800

    xiufu秘钥问题

---
+++ socialuni-js/packages/socialuni-native/socialuni-native-h5/src/util/UniappInner.ts
@@ -0,0 +1,128 @@
+const MIMEType = {
+    /**
+     * 关于图片常见的MIME类型
+     */
+    image: {
+        jpg: 'jpeg',
+        jpe: 'jpeg',
+        pbm: 'x-portable-bitmap',
+        pgm: 'x-portable-graymap',
+        pnm: 'x-portable-anymap',
+        ppm: 'x-portable-pixmap',
+        psd: 'vnd.adobe.photoshop',
+        pic: 'x-pict',
+        rgb: 'x-rgb',
+        svg: 'svg+xml',
+        svgz: 'svg+xml',
+        tif: 'tiff',
+        xif: 'vnd.xiff',
+        wbmp: 'vnd.wap.wbmp',
+        wdp: 'vnd.ms-photo',
+        xbm: 'x-xbitmap',
+        ico: 'x-icon'
+    },
+    /**
+     * 关于视频常见的MIME类型
+     */
+    video: {
+        '3g2': '3gpp2',
+        '3gp': '3gpp',
+        avi: 'x-msvideo',
+        f4v: 'x-f4v',
+        flv: 'x-flv',
+        jpgm: 'jpm',
+        jpgv: 'jpeg',
+        m1v: 'mpeg',
+        m2v: 'mpeg',
+        mpe: 'mpeg',
+        mpg: 'mpeg',
+        mpg4: 'mpeg',
+        m4v: 'x-m4v',
+        mkv: 'x-matroska',
+        mov: 'quicktime',
+        qt: 'quicktime',
+        movie: 'x-sgi-movie',
+        mp4v: 'mp4',
+        ogv: 'ogg',
+        smv: 'x-smv',
+        wm: 'x-ms-wm',
+        wmv: 'x-ms-wmv',
+        wmx: 'x-ms-wmx',
+        wvx: 'x-ms-wvx'
+    }
+}
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+
+export default class UniappInner {
+    static hasOwn (obj, key) {
+        return hasOwnProperty.call(obj, key)
+    }
+    /**
+     * 从本地file或者blob对象创建url
+     * @param {Blob|File} file
+     * @return {string}
+     */
+    static fileToUrl (file) {
+        const files = {}
+        for (const key in files) {
+            if (this.hasOwn(files, key)) {
+                const oldFile = files[key]
+                if (oldFile === file) {
+                    return key
+                }
+            }
+        }
+        var url = (window.URL || window.webkitURL).createObjectURL(file)
+        files[url] = file
+        return url
+    }
+
+    static updateElementStyle(element, styles) {
+        for (const attrName in styles) {
+            element.style[attrName] = styles[attrName]
+        }
+    }
+
+    static createFileInput({count, sourceType, type, extension}) {
+        const inputEl = document.createElement('input')
+        inputEl.type = 'file'
+
+        this.updateElementStyle(inputEl, {
+            position: 'absolute',
+            visibility: 'hidden',
+            'z-index': -999,
+            width: 0,
+            height: 0,
+            top: 0,
+            left: 0
+        })
+        const ALL = '*'
+        /**
+         * 选择文件
+         * chooseFile 使用后缀名
+         * chooseImage、chooseVideo 使用MIME类型
+         */
+        inputEl.accept = extension.map(item => {
+            if (type !== ALL) {
+                const MIMEKey = item.replace('.', '')
+                return `${type}/${MIMEType[type][MIMEKey] || MIMEKey}`
+            } else {
+                return item.indexOf('.') === 0 ? item : `.${item}`
+            }
+        }).join(',')
+
+        if (count > 1) {
+            inputEl.multiple = 'multiple'
+        }
+
+        // 经过测试，仅能限制只通过相机拍摄，不能限制只允许从相册选择。
+        if (sourceType.length === 1 && sourceType[0] === 'camera') {
+            inputEl.capture = 'camera'
+        }
+
+        return inputEl
+    }
+
+}
---
+++ socialuni-js/packages/socialuni-user/socialuni-user-view-h5/src/views/SocialuniLoginView.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="row-all-center pd">
+        <div class="w300">
+            <el-form
+                    ref="loginForm"
+                    :model="viewService.loginUser"
+                    :rules="viewService.loginRules"
+                    auto-complete="on"
+                    label-position="top"
+            >
+                <el-form-item label="手机号" prop="phoneNum" label-width="70px">
+                    <div class="flex-row w100p">
+                        <el-input
+                                class="flex-1"
+                                v-model="viewService.loginUser.phoneNum"
+                                placeholder="请输入手机号"
+                                type="text"
+                                :maxlength="11"
+                                clearable
+                        >
+                            <template #prepend>+86</template>
+                        </el-input>
+                    </div>
+                </el-form-item>
+                <!--          <el-form-item label="密码" prop="phoneNum" label-width="70px">
+                  <div class="flex-row">
+                    <el-input
+                      v-model="loginUser.phoneNum"
+                      placeholder="请输入密码"
+                      type="text"
+                      :maxlength="11"
+                      clearable
+                    />
+                  </div>
+                </el-form-item>-->
+                <el-form-item label="验证码" prop="authCode" label-width="70px">
+                    <div class="flex-row w100p">
+                        <el-input
+                                ref="authCode"
+                                v-model="viewService.loginUser.authCode"
+                                class="flex-1"
+                                type="text"
+                                placeholder="请输入验证码"
+                                :maxlength="4"
+                                clearable
+                                @keyup.enter.native="viewService.handleLogin()"
+                        />
+                        <el-button
+                                class="flex-none ml w130"
+                                plain
+                                type="primary"
+                                :disabled="viewService.sendAuthCodeBtnDisabled"
+                                @click="viewService.sendCodeClick()"
+                        >
+                            {{
+                            viewService.countDown ? (viewService.authCodeInterval + 1 - viewService.countDown) + '秒后可重试' : '发送验证码'
+                            }}
+                        </el-button>
+                    </div>
+                </el-form-item>
+
+                <div class="mt-30">
+                    <el-checkbox v-model="viewService.contractChecked" class="mb-5">
+                        <span class="color-black">我已阅读并同意</span>
+                    </el-checkbox>
+                    <a href="https://www.qingchiapp.com/agreement" class="color-blue">
+                        《用户协议》
+                    </a>
+                    <a href="https://www.qingchiapp.com/privacy" class="color-blue">
+                        《隐私政策》
+                    </a>
+                    <a href="https://www.qingchiapp.com/childProtect" class="color-blue">
+                        《儿童个人信息保护规则及监护人须知》
+                    </a>
+                </div>
+
+                <el-button
+                        :disabled="viewService.bindBtnDisabled"
+                        type="primary"
+                        class="w100p mt"
+                        @click.native.prevent="viewService.handleLogin()"
+                >{{ viewService.bindBtnDisabled ? '登陆中' : '注 册 并 登 录' }}
+                </el-button>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Emit, Options, Vue} from 'vue-property-decorator'
+import SocialuniLoginViewService from "@socialuni/socialuni-user-sdk/src/logic/SocialuniLoginViewService";
+
+@Options({})
+export default class SocialuniLoginView extends Vue {
+    viewService: SocialuniLoginViewService = new SocialuniLoginViewService()
+
+    created() {
+        this.viewService.initService(this as any)
+    }
+}
+</script>
