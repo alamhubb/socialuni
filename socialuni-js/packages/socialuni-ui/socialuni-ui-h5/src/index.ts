2fb0bbae7d00e56a261acc738f9c9ee1a38038ad
socialuni <qinky94@163.com>
2023-08-06 23:03:22 +0800

    支持编译小程序

---
+++ socialuni-js/packages/socialuni-ui/socialuni-ui-h5/src/index.ts
@@ -0,0 +1,16 @@
+import {App} from "vue";
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import SocialuniScss from '@socialuni/socialuni-scss/src/index'
+import 'element-plus/dist/index.css'
+
+const SocialuniUiH5 = {
+    async install(app: App) {
+        app.use(SocialuniScss)
+        app.use(ElementPlus, {
+            locale: zhCn,
+        })
+    }
+}
+
+export default SocialuniUiH5
