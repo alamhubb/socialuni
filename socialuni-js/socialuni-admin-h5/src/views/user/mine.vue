e84c1b92c3806f41cd997f8646987ef6f42f5a37
socialuni <socialuni@163.com>
2023-08-31 12:38:46 +0800

    admin h5

---
+++ socialuni-js/socialuni-admin-h5/src/views/user/mine.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="h100p overflow-hidden row-all-center">
+        用户详情
+    </main>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-property-decorator";
+
+@Options({
+    components: {}
+})
+export default class MinePage extends Vue {
+
+
+}
+</script>
