8a8d24787b31fed29252cae3aea7640bfc9d84d2
socialuni <socialuni@163.com>
2023-09-04 17:40:30 +0800

    admin h5

--- socialuni-js/socialuni-admin-h5/src/router/router.ts
+++ socialuni-js/socialuni-admin-h5/src/router/router.ts
@@ -1,6 +1,6 @@
 // 1. 定义路由组件.
 // 也可以从其他文件导入
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 
 import Layout from '@/layout/Layout.vue'
 import RouterName from '@/constant/RouterName'
@@ -124,7 +124,7 @@ console.log(constantRoutes)
 // 暂时保持简单
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: constantRoutes, // `routes: routes` 的缩写
 })
 
