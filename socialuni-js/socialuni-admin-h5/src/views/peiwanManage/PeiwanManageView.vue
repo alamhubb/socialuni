bfd1c0b1093caa3b63eb37c3490fc2ded8d24f4e
socialuni <qinky94@163.com>
2023-09-06 00:11:31 +0800

    init

--- socialuni-js/socialuni-admin-h5/src/views/peiwanManage/PeiwanManageView.vue
+++ socialuni-js/socialuni-admin-h5/src/views/peiwanManage/PeiwanManageView.vue
@@ -1,36 +1,43 @@
 <template>
-    <div class="h100p">
-        <div class="flex-row">
-            <!--            <label-item label="陪玩名称">
-                            <el-input></el-input>
-                        </label-item>-->
+  <div class="h100p">
+    <div class="flex-row">
+      <!--            <label-item label="陪玩名称">
+                      <el-input></el-input>
+                  </label-item>-->
 
-            <!--            <label-item label="图片">
+      <!--            <label-item label="图片">
 
-                        </label-item>-->
-            <el-upload drag class="size100 overflow-hidden bd-radius" :auto-upload="false" :on-change="avatarImgChange">
-                <el-image v-if="peiwanInfo.avatar" class="size100" :src="peiwanInfo.avatar"
-                          fit="fill"/>
-                <el-icon v-else class="el-icon&#45;&#45;upload size100">
-                    <Plus/>
-                </el-icon>
-            </el-upload>
-
-            <!--            <label-item label="位置和经纬度：">
-                            <el-button class="mr-sm" size="small" type="primary" @click="openMapDialog">选择位置</el-button>
-                            <div>
-                                {{ peiwanInfo.district }}：{{ peiwanInfo.lat }}，{{ peiwanInfo.lng }}
-                            </div>
-                        </label-item>-->
+                  </label-item>-->
+      <el-upload drag class="size100 overflow-hidden bd-radius" :auto-upload="false" :on-change="avatarImgChange">
+        <div v-if="peiwanInfo.avatar" class=" position-relative">
+          <el-image class="size100 overflow-hidden" :src="peiwanInfo.avatar"
+                    fit="fill"/>
+          <div
+              class="position-absolute z-index-button color-white size20 bg-grey8 bg-half-transparent row-all-center topRight bd bbl-radius"
+              @click="deleteImg(index)">×
+          </div>
         </div>
 
-        <s-dialog title="选择位置" ref="mapDialog" width="70%" top="3vh" dialog-body-height="450px">
-            <iframe id="mapPage" width="100%" height="420px" frameborder=0
-                    src="https://apis.map.qq.com/tools/locpicker?search=1&type=1&policy=1&key=FZDBZ-5XCLO-OCYW6-SRG4Y-ZFP7O-IJBSD&referer=peiwan">
-            </iframe>
-        </s-dialog>
+        <el-icon v-else class="el-icon&#45;&#45;upload size100">
+          <Plus/>
+        </el-icon>
+      </el-upload>
 
+      <!--            <label-item label="位置和经纬度：">
+                      <el-button class="mr-sm" size="small" type="primary" @click="openMapDialog">选择位置</el-button>
+                      <div>
+                          {{ peiwanInfo.district }}：{{ peiwanInfo.lat }}，{{ peiwanInfo.lng }}
+                      </div>
+                  </label-item>-->
     </div>
+
+    <s-dialog title="选择位置" ref="mapDialog" width="70%" top="3vh" dialog-body-height="450px">
+      <iframe id="mapPage" width="100%" height="420px" frameborder=0
+              src="https://apis.map.qq.com/tools/locpicker?search=1&type=1&policy=1&key=FZDBZ-5XCLO-OCYW6-SRG4Y-ZFP7O-IJBSD&referer=peiwan">
+      </iframe>
+    </s-dialog>
+
+  </div>
 </template>
 
 <script lang="ts">
@@ -50,80 +57,80 @@ import CosService from "@socialuni/socialuni-app-sdk/src/util/CosService";
 import UUIDUtil from "@/utils/UUIDUtil";
 
 @Component({
-    components: {LabelItem, SDialog, Plus}
+  components: {LabelItem, SDialog, Plus}
 })
 export default class PeiwanManageView extends Vue {
-    $refs: {
-        mapDialog: SDialog
-    }
+  $refs: {
+    mapDialog: SDialog
+  }
 
-    openMapDialog() {
-        this.$refs.mapDialog.open()
-    }
+  openMapDialog() {
+    this.$refs.mapDialog.open()
+  }
 
-    peiwanUuid = UUIDUtil.getUUID()
+  peiwanUuid = UUIDUtil.getUUID()
 
-    created() {
-        this.peiwanUuid = UUIDUtil.getUUID()
-    }
+  created() {
+    this.peiwanUuid = UUIDUtil.getUUID()
+  }
 
-    district = null
+  district = null
 
-    peiwanInfo: PeiwanRO = new PeiwanRO()
+  peiwanInfo: PeiwanRO = new PeiwanRO()
 
 
-    beijingDistrict = [
-        '东城',
-        '西城',
-        '朝阳',
-        '丰台',
-        '石景山',
-        '海淀',
-        '房山',
-        '通州',
-        '顺义',
-        '昌平',
-        '大兴'
-    ]
+  beijingDistrict = [
+    '东城',
+    '西城',
+    '朝阳',
+    '丰台',
+    '石景山',
+    '海淀',
+    '房山',
+    '通州',
+    '顺义',
+    '昌平',
+    '大兴'
+  ]
 
-    async avatarImgChange(file: any) {
-        const imgFile: DomFile = FileUtilH5.fileSetPath(file.raw)
+  async avatarImgChange(file: any) {
+    const imgFile: DomFile = FileUtilH5.fileSetPath(file.raw)
 
-        this.peiwanInfo.avatar = imgFile.path
+    this.peiwanInfo.avatar = imgFile.path
 
-        const cosAuthRO = await CosService.getCosAuthRO()
+    const cosAuthRO = await CosService.getCosAuthRO()
 
-        await ImgUtilH5.setImgQualityAndAspectRatio(imgFile)
+    await ImgUtilH5.setImgQualityAndAspectRatio(imgFile)
 
-        imgFile.src = cosAuthRO.uploadImgPath + `manage/${this.peiwanUuid}/` + imgFile.src
+    imgFile.src = cosAuthRO.uploadImgPath + `manage/${this.peiwanUuid}/` + imgFile.src
 
-        console.log(imgFile)
+    console.log(imgFile)
 
-        const res = await TencentCosAPI.uploadFileAPI(imgFile, cosAuthRO)
+    const res = await TencentCosAPI.uploadFileAPI(imgFile, cosAuthRO)
 
-        console.log(res)
-        // this.peiwanInfo.avatar = imgFile.src
-    }
+    console.log(res)
+    // this.peiwanInfo.avatar = imgFile.src
+  }
 
-    created() {
-        console.log('进入了')
-        window.addEventListener('message', (event) => {
-            // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
-            const loc = event.data
-            if (loc) {
-                if (loc.module === 'locationPicker') {
-                    if (loc.latlng) {
-                        this.peiwanInfo.lat = loc.latlng.lat
-                        this.peiwanInfo.lng = loc.latlng.lng
-                    }
-                    for (const district of this.beijingDistrict) {
-                        if (loc.poiaddress && loc.poiaddress.includes(district)) {
-                            this.peiwanInfo.district = district
-                        }
-                    }
-                }
+  created() {
+    console.log('进入了')
+    window.addEventListener('message', (event) => {
+      // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
+      const loc = event.data
+      if (loc) {
+        if (loc.module === 'locationPicker') {
+          if (loc.latlng) {
+            this.peiwanInfo.lat = loc.latlng.lat
+            this.peiwanInfo.lng = loc.latlng.lng
+          }
+          for (const district of this.beijingDistrict) {
+            if (loc.poiaddress && loc.poiaddress.includes(district)) {
+              this.peiwanInfo.district = district
             }
-        }, false)
-    }
+          }
+        }
+      }
+    }, false)
+  }
 }
 </script>
