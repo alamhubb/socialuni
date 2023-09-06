974fc74ecb09f8cdafcfad3357165da02261f26f
socialuni <socialuni@163.com>
2023-09-06 16:51:25 +0800

    支持更新头像

--- socialuni-js/socialuni-peiwan-h5/src/router/router.ts
+++ socialuni-js/socialuni-peiwan-h5/src/router/router.ts
@@ -2,69 +2,6 @@
 // 也可以从其他文件导入
 import {createRouter,  createWebHistory} from "vue-router";
 
-import Layout from '@/layout/Layout.vue'
-import {socialuniUserModule} from "@socialuni/socialuni-user-sdk/src/store/SocialuniUserModule";
-import SocialuniTokenUtil from "@socialuni/socialuni-user-sdk/src/util/SocialuniTokenUtil";
-import PeiwanManageView from '@/views/peiwanManage/PeiwanManageView.vue'
-
-export const menuRoutes = [
-    /*{
-      path: '',
-      name: RouterName.devAccount,
-      component: () => import('@/views/devInfo/devInfo.vue'),
-      meta: { title: '开发者信息', icon: 'strengthMonitoring', isOpen: true }
-    },
-    {
-      path: 'contentAudit',
-      name: RouterName.contentAudit,
-      component: () => import('@/views/audit/contentAudit.vue'),
-      // children: showRouterList,
-      meta: { title: RouterName.contentAudit, icon: 'strengthMonitoring', isOpen: true }
-    },
-    {
-      path: '/keywordManage',
-      name: RouterName.keywordsManage,
-      component: () => import('@/views/keywordManage/keywordManage.vue'),
-      meta: { title: RouterName.keywordsManage, icon: 'strengthMonitoring' }
-    },
-    {
-      path: '/configManage',
-      name: RouterName.configManage,
-      component: () => import('@/views/configManage/configManage.vue'),
-      meta: { title: RouterName.configManage, icon: 'strengthMonitoring', isOpen: true }
-    },
-    {
-      path: '/homeSwiperManage',
-      name: RouterName.homeSwiperManage,
-      component: () => import('@/views/homeSwiperManage/homeSwiperManage.vue'),
-      meta: { title: RouterName.homeSwiperManage, icon: 'strengthMonitoring', isOpen: true }
-    },
-    {
-      path: '/stickTalkManage',
-      name: RouterName.stickTalkManage,
-      component: () => import('@/views/stickTalkManage/stickTalkManage.vue'),
-      meta: { title: RouterName.stickTalkManage, icon: 'strengthMonitoring', isOpen: true }
-    },
-    {
-      path: '/imgAuditHistory',
-      name: RouterName.imgAuditHistory,
-      component: () => import('@/views/audit/imgAuditHistory.vue'),
-      meta: { title: RouterName.imgAuditHistory, icon: 'strengthMonitoring' }
-    },
-    {
-      path: '/userIdentityAudit',
-      name: RouterName.userIdentityAudit,
-      component: () => import('@/views/userIdentityAudit/UserIdentityAuditView.vue'),
-      meta: { title: RouterName.userIdentityAudit, icon: 'strengthMonitoring' }
-    },*/
-    {
-        path: '/peiwanManage',
-        name: 'peiwanManage',
-        component: PeiwanManageView,
-        meta: {title: '陪玩管理', icon: 'strengthMonitoring'}
-    }
-]
-
 export const constantRoutes = [
     {
         path: '/404',
@@ -75,44 +12,13 @@ export const constantRoutes = [
         path: '/login',
         component: () => import('@/views/login/login.vue'),
         hidden: true
-    },
-    {
+    },  {
         path: '/',
-        component: Layout,
-        redirect: '/peiwanManage',
-        children: menuRoutes
+        component: () => import('@/views/home/HomePage.vue'),
+        hidden: true
     },
     // 404 page must be placed at the end !!!
     {path: '/:pathMatch(.*)', redirect: '/404', hidden: true}
-    /* {
-      path: '/',
-      component: Layout,
-      redirect: '/',
-      children: [
-        {
-          path: '/' + RouterName.auditHistory,
-          name: RouterName.auditHistory,
-          // redirect: '/review',
-          component: () => import('@/views/audit/auditHistory.vue'),
-          // children: showRouterList,
-          meta: { title: '审核历史', icon: 'strengthMonitoring' }
-        }]
-    },{
-      path: '/',
-      component: Layout,
-      redirect: '/',
-      children: [
-        {
-          path: '/test',
-          name: '测试',
-          component: () => import('@/views/testTemp/UserIdentityAuditView.vue'),
-          meta: { title: '测试', icon: 'strengthMonitoring' }
-        }]
-    },*/
-    /* {
-      path: 'https://socialuni.cn/',
-      meta: { title: '文档' }
-    },*/
 ]
 
 console.log(constantRoutes)
@@ -130,34 +36,7 @@ const router = createRouter({
 const whiteList = ['/login'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-    // set page title
-    let user = socialuniUserModule.mineUser
-    console.log(user)
-    const hasToken = socialuniUserModule.hasToken
-    if (!user) {
-        if (hasToken) {
-            socialuniUserModule.getUserInfo()
-        }
-    }
-    console.log(hasToken)
-    if (hasToken) {
-        if (to.path === '/login') {
-            // if is logged in, redirect to the home page
-            next({ path: '/' })
-        } else {
-            console.log(to.path)
-            next()
-        }
-    } else {
-        /* has no token*/
-        if (whiteList.indexOf(to.path) !== -1) {
-            // in the free login whitelist, go directly
-            next()
-        } else {
-            // other pages that do not have permission to access are redirected to the login page.
-            next({ path: '/login' })
-        }
-    }
+
 })
 
 router.afterEach(() => {
---
+++ socialuni-js/socialuni-peiwan-h5/src/views/home/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="h100p">
+
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-facing-decorator'
+import {DialogInterface} from "@/interface/DialogInterface";
+import PeiwanRO from "@socialuni/socialuni-admin-api/src/model/peiwan/PeiwanRO";
+import SDialog from "@socialuni/socialuni-ui-h5/src/components/SComponents/SDialog.vue";
+import {Plus} from '@element-plus/icons-vue'
+import FileUtilH5 from "@socialuni/socialuni-native-h5/src/util/FileUtilH5";
+import type DomFile from "@socialuni/socialuni-native-util/src/model/DomFile";
+import ImgUtilH5 from "@socialuni/socialuni-native-h5/src/util/ImgUtilH5";
+import TencentCosAPI from "@socialuni/socialuni-app-api/src/api/TencentCosAPI";
+import CosService from "@socialuni/socialuni-app-sdk/src/util/CosService";
+import UUIDUtil from "@/utils/UUIDUtil";
+import SocialuniPeiwanAPI from "@socialuni/socialuni-peiwan-api/src/api/SocialuniPeiwanAPI";
+import {useMagicKeys} from '@vueuse/core'
+import {watch} from "vue";
+import AlertUtil from "@socialuni/socialuni-native-h5/src/util/AlertUtil";
+
+
+@Component({
+    components: {SDialog, Plus}
+})
+export default class PeiwanManageView extends Vue {
+    $refs: {
+        mapDialog: SDialog
+    }
+
+
+}
+</script>
