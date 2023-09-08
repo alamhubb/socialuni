c9b7db45eabd69e0647e272f1698e2155372db50
socialuni <socialuni@163.com>
2023-09-08 17:54:48 +0800

    支持更新头像

---
+++ socialuni-js/socialuni-music-h5/src/layout/components/HorizontalSidebar/Logo.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+    <transition name="sidebarLogoFade">
+      <router-link key="collapse" class="sidebar-logo-link" to="/">
+        <img :src="logo" class="sidebar-logo">
+        <h1 class="sidebar-title">{{ title }} </h1>
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<script>
+import logoImg from '@/assets/img/logo.png'
+export default {
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: '数据资源融聚',
+      logo: logoImg
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
+}
+
+.sidebar-logo-container {
+  position: relative;
+  width: 250px;
+  height: 60px;
+  line-height: 60px;
+  background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
+  float: left;
+  z-index: 909;
+
+  & .sidebar-logo-link {
+    height: 100%;
+    width: 100%;
+
+    & .sidebar-logo {
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+      margin-right: 12px;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      margin: 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 50px;
+      font-size: 14px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+      vertical-align: middle;
+    }
+  }
+
+  &.collapse {
+    .sidebar-logo {
+      margin-right: 0px;
+    }
+  }
+}
+</style>
---
+++ socialuni-js/socialuni-music-h5/src/views/home/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="h100p">
+
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-facing-decorator'
+import SDialog from "@socialuni/socialuni-ui-h5/src/components/SComponents/SDialog.vue";
+import {Plus} from '@element-plus/icons-vue'
+import SocialuniPeiwanAPI from "@socialuni/socialuni-peiwan-api/src/api/SocialuniPeiwanAPI";
+
+@Component({
+    components: {SDialog, Plus}
+})
+export default class HomePage extends Vue {
+    $refs: {
+        mapDialog: SDialog
+    }
+
+    created(){
+        SocialuniPeiwanAPI.queryPeiwanInfoListAPI()
+    }
+
+}
+</script>
