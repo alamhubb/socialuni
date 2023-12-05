d4bdafa47f4c6c66e68335db8bc3ac7f449cfad3
socialuni <socialuni@163.com>
2023-12-05 10:32:41 +0800

    抽象websocket

--- socialuni-js/socialuni-example-h5/src/views/chat/MessageView.ts
+++ socialuni-js/socialuni-example-h5/src/views/chat/MessageView.ts
@@ -3,15 +3,9 @@ import {div} from "@/views/chat/VueRender";
 export default {
     setup() {
         // 使用数组返回多个根节点
-        const haha = () => [
-            div(123,
-                [
-                    div(33),
-                    div(333),
-                    div(333),
-                ]
-            ),
+        return () => [
+            div([123, div('123'), div('123')]),
+            div('123')
         ]
-        return haha
     }
 }
