8a152340cb5cbb5122787bc358afd88f787a956e
socialuni <socialuni@163.com>
2023-12-07 09:58:42 +0800

    开发 嵌套功能

--- socialuni-js/socialuni-music-h5/src/views/chat/MessageView.vue
+++ socialuni-js/socialuni-music-h5/src/views/chat/MessageView.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="flex-row h100p overflow-hidden pd bg-white">
-    <div class="flex-row overflow-hidden">
-      <div v-for="i in 20" class="flex-1 w200 h30" :class="[active===i?'bg-blue_light bb-2 bb-blue':'bg-default']">
+  <div class="flex-col h100p overflow-hidden pd bg-white">
+    <div class="flex-row overflow-hidden flex-none">
+      <div v-for="i in 20" class="flex-1 w200 h30" :class="[active===i?'bg-blue_light bb-2 bb-blue':'bg-default']" @click="active=i">
         {{ i }}
       </div>
+    </div>
+    <div class="flex-1 overflow-hidden bg-green_light pd">
+    {{ active}}
     </div>
   </div>
 </template>
