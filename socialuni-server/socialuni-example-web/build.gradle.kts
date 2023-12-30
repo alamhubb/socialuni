c4ee6852453e9eced33305b2f8dbcd3697140f07
socialuni <socialuni@163.com>
2023-12-31 01:54:39 +0800

    支持启动web项目

--- socialuni-server/socialuni-example-web/build.gradle.kts
+++ socialuni-server/socialuni-example-web/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.socialuni.social:socialuni-sdk")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("jakarta.persistence:jakarta.persistence-api")
