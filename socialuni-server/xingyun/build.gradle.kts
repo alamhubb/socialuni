7a07d3fe31d97b5cf3321beb17acb96e8920a17c
socialuni <socialuni@163.com>
2023-12-30 15:49:04 +0800

    添加kotlin项目

--- socialuni-server/xingyun/build.gradle.kts
+++ socialuni-server/xingyun/build.gradle.kts
@@ -1,9 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenLocal()
@@ -16,13 +31,28 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("com.socialuni.social:socialuni-common-sdk:0.3.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
-tasks.test {
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
---
+++ socialuni-server/xingyun/src/main/kotlin/XingYunApplication.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XingYunApplication
+
+fun main(args: Array<String>) {
+    runApplication<XingYunApplication>(*args)
+}
