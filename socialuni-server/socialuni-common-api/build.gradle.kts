b54dc45428bf4679c5412ecfd63eb772f50a7083
socialuni <socialuni@163.com>
2023-12-30 21:56:02 +0800

    123

--- socialuni-server/socialuni-common-api/build.gradle.kts
+++ socialuni-server/socialuni-common-api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
--- socialuni-server/socialuni-common-api/src/main/java/com/example/demo/Demo1Application.java
+++ socialuni-server/socialuni-common-api/src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Demo1Application {
 
     public static void main(String[] args) {
         System.out.println(new TestRO());
-        SpringApplication.run(Demo1Application.class, args);
+//        SpringApplication.run(Demo1Application.class, args);
     }
 
 }
