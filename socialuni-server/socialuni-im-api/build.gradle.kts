ccc19551d1ffaeca6cb1128ea9f06b1ad08d334d
socialuni <socialuni@163.com>
2023-12-31 01:34:56 +0800

    支持websdk，community-sdk

--- socialuni-server/socialuni-im-api/build.gradle.kts
+++ socialuni-server/socialuni-im-api/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     implementation("com.socialuni.social:socialuni-common-api")
     implementation("io.github.openfeign:feign-httpclient")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("io.swagger.core.v3:swagger-core")
 }
