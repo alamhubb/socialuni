8e7df113b639834f037aca7aa0ffdb59a376f7df
socialuni <socialuni@163.com>
2024-01-05 00:58:01 +0800

    支持显示歌曲

---
+++ socialuni-js/socialuni-im-h5/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h100p overflow-hidden flex-col bg-default">
+        <nav-bar/>
+        <div class="flex-1 overflow-hidden">
+            <RouterView/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator';
+import NavBar from "@/layout/NavBar.vue";
+
+@Component({
+    components: {NavBar}
+})
+export default class App extends Vue {
+}
+</script>
