b6c48d1cc0657acabcd15abfd06156f3ec682553
socialuni <socialuni@163.com>
2024-01-30 17:12:19 +0800

    支持成功部署文件了

--- socialuni-js/socialuni-velox-h5/src/views/chat/fsafd.js
+++ socialuni-js/socialuni-velox-h5/src/views/chat/fsafd.js
@@ -1,15 +1,24 @@
 const ary = []
 const ary1 = []
+const res = []
 for (let i = 97; i <= 122; i++) {
-    ary.push(String.fromCharCode(i).repeat(3));
-    ary1.push(String.fromCharCode(i).repeat(3));
+    ary.push(String.fromCharCode(i).repeat(2));
+    ary1.push(String.fromCharCode(i).repeat(2));
 }
 for (let i = 1; i < 10; i++) {
-    ary1.push(String(i).repeat(3));
+    ary1.push(String(i).repeat(2));
 }
 for (let i = 2; i < 9; i++) {
-    ary1.push(String(i - 1) + String(i) + String(i + 1));
+    ary1.push(String(i - 1) + String(i));
 }
-console.log(ary.length)
-console.log(ary)
-console.log(ary1)
+for (const aryElement of ary) {
+    for (const ary1Element of ary1) {
+        if (aryElement !== ary1Element) {
+            res.push(aryElement + ary1Element)
+        }
+    }
+}
+
+console.log(res.length)
+// console.log(JSON.stringify(res))
+
