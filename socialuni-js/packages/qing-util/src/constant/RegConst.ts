8b1f6b27ccb9fc2079d97d5473b557e298337d0e
socialuni <socialuni@163.com>
2024-01-31 21:24:30 +0800

    支持同步生成测试账号

--- socialuni-js/packages/qing-util/src/constant/RegConst.ts
+++ socialuni-js/packages/qing-util/src/constant/RegConst.ts
@@ -1,7 +1,7 @@
 export default class RegConst {
     static startWithNumber = /^[0-9]/;
     static startWithNumberOrLetter = /^[0-9a-zA-Z]/;
-    static startWithLetter = /^[a-z]/;
-    static startWithNotLetter = /^[^a-z]/;
+    static startWithLetter = /^[a-zA-Z]/;
+    static startWithNotLetter = /^[^a-zA-Z]/;
     static isAllLetter = /^[a-zA-Z]+$/;
 }
