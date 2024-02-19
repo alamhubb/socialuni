44200f100f12489cb0e3ed3f074067ed427a938c
socialuni <socialuni@163.com>
2024-02-20 01:27:05 +0800

    修复发送验证码功能

--- socialuni-js/packages/socialuni-user/socialuni-user-sdk/src/model/SocialuniLoginFormDataVO.ts
+++ socialuni-js/packages/socialuni-user/socialuni-user-sdk/src/model/SocialuniLoginFormDataVO.ts
@@ -19,36 +19,15 @@ export default class SocialuniLoginFormDataVO {
     passwordFocus = false
     authCodeFocus = false
     phoneNumFocus = false
-    phoneNumFirstBlur = false
 
-    checkPhoneNum() {
-        // 再次校验
-        if (this.phoneNumHasError) {
-            return QingAppUtil.ToastUtil.error('请输入正确的手机号')
-        }
-    }
-
-    checkPassword() {
-        if (this.passwordHasError) {
-            return QingAppUtil.ToastUtil.error(this.passwordHasError)
-        }
-    }
-
-    checkAuthCode() {
-        if (!this.phoneNumRegistered && this.authCodeHasError) {
-            return QingAppUtil.ToastUtil.error('请输入正确的验证码')
-        }
-    }
 
     get phoneNumHasError() {
         //有值，错误，才算错误
-        return this.phoneNumFirstBlur && (!this.phoneNum || this.phoneNum.length !== 11 || NumberUtil.containNoNumber(this.phoneNum))
+        return !this.phoneNum || this.phoneNum.length !== 11 || NumberUtil.containNoNumber(this.phoneNum)
     }
 
     get passwordHasError() {
-        if (!this.phoneNumFirstBlur || this.phoneNumHasError) {
-            return false
-        } else if (!this.password) {
+        if (!this.password) {
             return '密码不能为空'
         } else if (this.password.length < 8) {
             return '密码长度不能小于8个字符'
@@ -63,11 +42,11 @@ export default class SocialuniLoginFormDataVO {
     }
 
     get authCodeHasError() {
-        return this.phoneNumFirstBlur && !this.phoneNumHasError && !this.passwordHasError && (!this.authCode || this.authCode.length !== 4 || NumberUtil.containNoNumber(this.authCode))
+        return !this.phoneNumHasError && !this.passwordHasError && (!this.authCode || this.authCode.length !== 4 || NumberUtil.containNoNumber(this.authCode))
     }
 
     get sendAuthCodeBtnDisabled() {
-        return !this.phoneNumFirstBlur || this.phoneNumHasError || !!this.passwordHasError || this.countDownInner
+        return this.phoneNumHasError || !!this.passwordHasError || this.countDownInner
     }
 
 
@@ -92,65 +71,33 @@ export default class SocialuniLoginFormDataVO {
     }
 
 
-    sendAuthCodeCheck() {
+    checkPhoneNum() {
+        // 再次校验
+        if (this.phoneNumHasError) {
+            return QingAppUtil.ToastUtil.error('请输入正确的手机号')
+        }
+    }
+
+    checkPassword() {
+        if (this.passwordHasError) {
+            return QingAppUtil.ToastUtil.error(this.passwordHasError)
+        }
+    }
+
+    checkAuthCode() {
+        if (!this.phoneNumRegistered && this.authCodeHasError) {
+            return QingAppUtil.ToastUtil.error('请输入正确的验证码')
+        }
+    }
+
+    checkSendAuthCode() {
         this.checkPhoneNum()
         if (this.countDownInner) {
             return QingAppUtil.ToastUtil.throwError('验证码发送频繁，请等待')
         }
     }
 
-
-    phoneNumInputFocus() {
-        this.phoneNumFocus = true
-        CommonUtil.delayTime(100).then(() => {
-            this.phoneNumFocus = true
-        })
-    }
-
-    phoneNumInputBlur() {
-        this.phoneNumFocus = false
-        this.phoneNumFirstBlur = true
-    }
-
-    authCodeInputFocus() {
-        this.authCodeFocus = true
-        CommonUtil.delayTime(100).then(() => {
-            this.authCodeFocus = true
-        })
-    }
-
-    passwordInputFocus() {
-        this.passwordFocus = true
-        CommonUtil.delayTime(100).then(() => {
-            this.passwordFocus = true
-        })
-    }
-
-    authCodeInputBlur() {
-        this.authCodeFocus = false
-    }
-
-    passwordInputBlur() {
-        this.passwordFocus = false
-    }
-
     resetAuthCodeCountDown() {
         this.countDownInner = 0
     }
-
-    phoneNumClear() {
-        this.phoneNum = null
-        this.phoneNumInputFocus()
-    }
-
-    passwordClear() {
-        this.password = null
-        this.passwordInputFocus()
-    }
-
-    authCodeClear() {
-        this.authCode = null
-        this.authCodeInputFocus()
-    }
-
 }
