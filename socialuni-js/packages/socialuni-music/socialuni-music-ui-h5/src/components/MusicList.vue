65c7f1928b885633f7e74d2e66558cd765118158
socialuni <socialuni@163.com>
2024-02-21 16:15:02 +0800

    支持音乐功能

--- socialuni-js/packages/socialuni-music/socialuni-music-ui-h5/src/components/MusicList.vue
+++ socialuni-js/packages/socialuni-music/socialuni-music-ui-h5/src/components/MusicList.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="w100p bg-white">
 
-
+<div>
+  <div v-for="item in tableData">
+    {{ item}}
+  </div>
+</div>
     <!--    <div class="flex-1 overflow-hidden">
     &lt;!&ndash;
           音量：{{ socialuniMusicStore.musicVolume }}
@@ -36,44 +40,47 @@
         </div>-->
 
     歌曲列表
-<!--    <el-table height="100%" :data="data" stripe highlight-current-row
-              @row-dblclick="handleCurrentChange">
-      <el-table-column prop="title" label="音乐标题" width="100" show-overflow-tooltip></el-table-column>
-      <el-table-column label="歌手" show-overflow-tooltip>
-        <template #default="scope">
-          {{ scope.row.author.join(' / ') }}
-        </template>
-      </el-table-column>
-      <el-table-column label="歌手专辑" show-overflow-tooltip>
-        <template #default="scope">
-          {{ scope.row.album }}
-        </template>
-      </el-table-column>
-      <el-table-column label="时长" width="100">
-        <template #default="scope">
-          {{ $DateUtil.convertToTime(scope.row.musicTime) }}
-        </template>
-      </el-table-column>
-    </el-table>-->
+    <!--    <el-table height="100%" :data="data" stripe highlight-current-row
+                  @row-dblclick="handleCurrentChange">
+          <el-table-column prop="title" label="音乐标题" width="100" show-overflow-tooltip></el-table-column>
+          <el-table-column label="歌手" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.author.join(' / ') }}
+            </template>
+          </el-table-column>
+          <el-table-column label="歌手专辑" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.album }}
+            </template>
+          </el-table-column>
+          <el-table-column label="时长" width="100">
+            <template #default="scope">
+              {{ $DateUtil.convertToTime(scope.row.musicTime) }}
+            </template>
+          </el-table-column>
+        </el-table>-->
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Emit, Model, Prop, Vue, Watch} from 'vue-facing-decorator';
-import CommonEventUtil from "qingjs/src/util/CommonEventUtil.ts";
-import SocialuniImEventKey from "socialuni-im-api/src/constant/SocialuniMusicEventConst.ts";
-import socialuniMusicStore from "socialuni-music-sdk/src/store/SocialuniMusicStore.ts";
-import AlertUtil from "qingjs-h5/src/util/AlertUtil.ts";
-import {nextTick} from "vue";
-import MusicPlayerSongInfoRO from "socialuni-music-sdk/src/model/MusicPlayerSongInfoRO.ts";
-import MusicPlayerSongPlayingInfoRO from "socialuni-music-sdk/src/model/MusicPlayerSongPlayingInfoRO.ts";
-import ToastUtil from "qingjs-h5/src/util/ToastUtil.ts";
-import DateUtil from "qing-util/src/util/DateUtil.ts";
+import musicRequest from "socialuni-music-sdk/src/plugins/musicRequest";
 
 @Component({
   components: {}
 })
 export default class MusicList extends Vue {
+
+  created(){
+    this.queryMusicList()
+  }
+
+  tableData = []
+
+  async queryMusicList() {
+    const data = await musicRequest.get('/playlist/detail?_t=1699363992&id=3778678') as any
+    this.tableData = data.playlist.tracks
+  }
 
 }
 </script>
--- socialuni-js/socialuni-music-h5/src/views/chat/MusicView.vue
+++ socialuni-js/socialuni-music-h5/src/views/chat/MusicView.vue
@@ -7,13 +7,13 @@
 import {Component, Vue, Watch} from 'vue-facing-decorator';
 import SocialuniChatViewH5 from "socialuni-im-view-h5/src/views/SocialuniChatViewH5.vue"
 import SocialuniMsgViewH5 from "socialuni-im-view-h5/src/views/SocialuniMsgViewH5.vue"
-import musicRequest from "@/plugins/musicRequest";
-import MusicPlayer from "../../../../packages/socialuni-music/socialuni-music-ui-h5/src/components/MusicPlayer.vue";
 import MusicPlayerSongPlayingInfoRO from "socialuni-music-sdk/src/model/MusicPlayerSongPlayingInfoRO.ts";
 import socialuniMusicStore from "socialuni-music-sdk/src/store/SocialuniMusicStore.ts";
 import SocialuniMusicRoleId from "socialuni-music-sdk/src/constant/SocialuniMusicRoleId.ts";
 import MusicPlayerSongInfoRO from "socialuni-music-sdk/src/model/MusicPlayerSongInfoRO.ts";
 import SocialuniMusicAPI from "socialuni-music-sdk/src/api/SocialuniMusicAPI.ts";
+import musicRequest from "socialuni-music-sdk/src/plugins/musicRequest";
+import MusicPlayer from "socialuni-music-ui-h5/src/components/MusicPlayer.vue";
 
 @Component({
   components: {MusicPlayer, SocialuniChatViewH5, SocialuniMsgViewH5}
