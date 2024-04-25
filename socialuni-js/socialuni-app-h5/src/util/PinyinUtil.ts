edc39631a0ae2a7d03a12cee142b7cecec783b76
socialuni <socialuni@163.com>
2024-04-25 19:38:57 +0800

    网站下方挂备案号

---
+++ socialuni-js/socialuni-app-h5/src/util/PinyinUtil.ts
@@ -0,0 +1,15 @@
+import pinyin from 'pinyin';
+
+export default class PinyinUtil {
+    static convertToFirstUpperPinyin(text) {
+        return pinyin(text, {
+            style: pinyin.STYLE_NORMAL // 获取普通拼音
+        }).map((word, index) => {
+            if (index) {
+                return word[0].charAt(0).toUpperCase() + word[0].slice(1);
+
+            }
+            return word
+        }).join("");
+    }
+}
