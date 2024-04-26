9cb64491a400fda38d2473b5bfcf469fd7401a41
socialuni <socialuni@163.com>
2024-04-26 15:46:41 +0800

    网站下方挂备案号

--- socialuni-js/create-vite/src/views/HomeView.vue
+++ socialuni-js/create-vite/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <h1>This is an about page666</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+</script>
