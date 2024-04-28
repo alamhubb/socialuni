f58efe4eb81d93d9422ae914679969ec4fd57efa
socialuni <socialuni@163.com>
2024-04-28 18:14:22 +0800

    支持kt

--- socialuni-server/socialuni-follow-sdk/src/main/kotlin/com/socialuni/social/friend/dao/repository/SocialuniFollowRepository.kt
+++ socialuni-server/socialuni-follow-sdk/src/main/kotlin/com/socialuni/social/friend/dao/repository/SocialuniFollowRepository.kt
@@ -13,20 +13,20 @@ interface SocialuniFollowRepository : JpaRepository<SocialuniUserFollowDO?, Int?
     //关注后用户缓存修改，一人+粉丝，一人+关注
     @Caching(
         evict = [CacheEvict(
-            cacheNames = CommonRedisKey.userById,
+            cacheNames = [CommonRedisKey.userById],
             key = "#follow.userId"
         ), CacheEvict(
-            cacheNames = CommonRedisKey.queryUserFollowUserIds,
+            cacheNames = [CommonRedisKey.queryUserFollowUserIds],
             key = "#follow.userId"
         ), CacheEvict(
-            cacheNames = CommonRedisKey.userById,
+            cacheNames = [CommonRedisKey.userById],
             key = "#follow.beUserId"
         ), CacheEvict(
-            cacheNames = CommonRedisKey.queryUserAndBeUserFollow,
+            cacheNames = [CommonRedisKey.queryUserAndBeUserFollow],
             key = "#follow.userId+'-'+#follow.beUserId"
         )]
     )
-    override fun save(follow: SocialuniUserFollowDO): SocialuniUserFollowDO
+    fun save(follow: SocialuniUserFollowDO): SocialuniUserFollowDO
 
     @Query("select f.beUserId from SocialuniUserFollowDO f where f.userId = :userId and f.status=:followStatus order by f.beUserId asc")
     fun queryUserFollowUserIds(
