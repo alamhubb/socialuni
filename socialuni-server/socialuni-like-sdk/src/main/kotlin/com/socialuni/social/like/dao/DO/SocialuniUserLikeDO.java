4c364e2163e6bff05dc247ab180e744a1730d924
socialuni <socialuni@163.com>
2024-05-07 14:44:11 +0800

    不再使用kotlin

---
+++ socialuni-server/socialuni-like-sdk/src/main/kotlin/com/socialuni/social/like/dao/DO/SocialuniUserLikeDO.java
@@ -0,0 +1,32 @@
+package com.socialuni.social.like.dao.DO;
+
+
+import com.socialuni.social.common.api.entity.SocialuniContentBaseDO;
+import com.socialuni.social.common.api.entity.SocialuniUserContactBaseDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+//用户扩展类
+@Entity
+@Table(name = "s_user_like",
+        //查询条件索引
+        indexes = {
+                //关联需要键索引，索引列不能为空
+                @Index(columnList = "userId"),
+                @Index(columnList = "beUserId"),
+                @Index(columnList = "status"),
+                @Index(columnList = "updateTime"),
+        },
+        uniqueConstraints = {
+                //每个渠道都是唯一的
+                @UniqueConstraint(columnNames = {"userId", "beUserId"}),
+        })
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class SocialuniUserLikeDO extends SocialuniUserContactBaseDO {
+}
