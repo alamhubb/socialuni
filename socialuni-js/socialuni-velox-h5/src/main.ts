92db7b16e078090e4685b43950867450958042bd
socialuni <socialuni@163.com>
2024-05-14 15:27:06 +0800

    是否存在诱导用户略过隐私政策链接的情形 首次运行时是否通过弹窗、突出链接等方式提示用户阅读隐私政策等收集使用规则

--- socialuni-js/socialuni-velox-h5/src/main.ts
+++ socialuni-js/socialuni-velox-h5/src/main.ts
@@ -6,6 +6,12 @@ import Socialuni from "socialuni/src";
 import router from "@/router";
 import SocialuniMusic from "socialuni-music-sdk/src";
 import DateUtil from "qing-util/src/util/DateUtil.ts";
+import SocialuniUiH5 from "socialuni-ui-h5/src";
+import SocialuniAppViewH5 from "socialuni-app-view-h5/src";
+import SocialuniUser from "socialuni-user-sdk/src";
+import SocialuniIm from "socialuni-im-sdk/src";
+import SocialuniExpandH5 from "socialuni-expand-view-h5/src";
+import SocialuniCommunityH5 from "socialuni-community-view-h5/src";
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
@@ -20,9 +26,12 @@ declare module '@vue/runtime-core' {
 
     app.config.globalProperties.$DateUtil = DateUtil;
 
-    await SocialuniMusic.install(app);
-    await Socialuni.install(app);
     app.use(router)
+    await Socialuni.install(app);
+    await SocialuniMusic.install(app);
+    app.use(SocialuniUiH5)
+    app.use(SocialuniAppViewH5)
+    app.use(SocialuniUser)
     app.mount('#app');
 })();
 
