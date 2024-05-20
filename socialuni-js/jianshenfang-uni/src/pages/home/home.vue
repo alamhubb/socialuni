89a5a665085b66376340ed495335410f03fa4f41
socialuni <socialuni@163.com>
2024-05-20 15:40:06 +0800

    修复图片问题

--- socialuni-js/jianshenfang-uni/src/pages/home/home.vue
+++ socialuni-js/jianshenfang-uni/src/pages/home/home.vue
@@ -1,52 +1,137 @@
 <template>
-  <div class="flex-col h100p bg-theme-bg">
-    <div class="bg-theme-gradual px pb-smm pt-100">
-      <div class="row-col-center">
-        <img
-            class="card-title-avatar bd bd-round flex-none"
-            mode="aspectFill"
-            src="https://cdxapp-1257733245.file.myqcloud.com/socialuni/common/app/defaultAvatar/girl/8326cffc1e178a8264b5aa6cb303738da877e876.jpg!avatar"
-        />
-        <div class="row-between-center flex-1">
-          <div class="row-col-center">
-            <span class="font-bold">明天的月光</span>
-            <social-gender-tag class="ml-xs" :user="{gender:'boy',age:20}"></social-gender-tag>
+  <div class="bg-theme-gradual px pb-smm pt-100">
+    <div class="row-col-center">
+      <img
+          class="card-title-avatar bd bd-round flex-none"
+          mode="aspectFill"
+          src="https://cdxapp-1257733245.file.myqcloud.com/socialuni/common/app/defaultAvatar/girl/8326cffc1e178a8264b5aa6cb303738da877e876.jpg!avatar"
+      />
+      <div class="row-between-center flex-1">
+        <div class="row-col-center">
+          <span class="font-bold font-18">明天的月光</span>
+          <social-gender-tag class="ml-xs" :user="{gender:'boy',age:20}"></social-gender-tag>
+        </div>
+        <div>
+          <div class="row-end-center">
+            <q-icon class="ml" icon="setting" size="20"></q-icon>
           </div>
-          <div>
-            <div class="row-end-center">
-              <q-icon class="ml" icon="setting" size="20"></q-icon>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt">
+      <div class="flex-row">
+        <!--          bg-orange3-->
+        <div class="flex-1 font-bold row-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-white">
+          <!--          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 mr-sm color-white" style="background-color: #ff8b36">-->
+          我的卡卷
+        </div>
+        <!--          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-red5 color-white">
+                    会员：200天
+                  </div>-->
+      </div>
+    </div>
+
+    <div>
+      <q-row-item class="px-sm">
+        <template #title>
+          <span class="font-16 font-bold">个人数据</span>
+        </template>
+        <div>我的名片</div>
+      </q-row-item>
+      <div class="mt-sm row-col-center py-10 bd-radius-10 mb-sm elevation-4 bg-white bg-orange_light py">
+        <div class="w60p flex-col">
+          <div class="row-around-center">
+            <div>
+              <div class="bg-orange color-white bd-round size30 row-all-center font-12">本月</div>
+            </div>
+            <div>
+              <div class="color-sub font-12">健身时长</div>
+              <div>
+                <span class="font-18 font-bold">0</span>
+                分钟
+              </div>
+            </div>
+            <div>
+              <div class="color-sub font-12">健身次数</div>
+              <div>
+                <span class="font-18 font-bold">0</span>
+                分钟
+              </div>
+            </div>
+          </div>
+          <div class="row-around-center mt">
+            <div>
+              <div class="bg-orange color-white bd-round size30 row-all-center font-12">本月</div>
+            </div>
+            <div>
+              <div class="color-sub font-12">健身时长</div>
+              <div>
+                <span class="font-18 font-bold">0</span>
+                分钟
+              </div>
+            </div>
+            <div>
+              <div class="color-sub font-12">健身次数</div>
+              <div>
+                <span class="font-18 font-bold">0</span>
+                分钟
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="mt">
-        <div class="flex-row px-xs">
-<!--          bg-orange3-->
-          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 mr-sm color-white" style="background-color: #ff8b36">
-            余额：500元
+        <div class="w40p col-all-center">
+          <div class="bd-round bg-white bd-orange size70 row-all-center">
+            <span class="font-20 font-bold">0</span> /100分
           </div>
-          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-red5 color-white">
-            会员：200天
+          <div class="bg-black color-white bd-radius-20 px-sm mt-xs font-12">本次训练评估</div>
+        </div>
+      </div>
+    </div>
+
+
+    <div>
+      <div class="font-16 font-bold px-sm">订单</div>
+      <div class="flex-row mt-sm">
+        <!--          bg-orange3-->
+        <div class="flex-1 col-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-white mr-sm">
+          <!--          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 mr-sm color-white" style="background-color: #ff8b36">-->
+          <div>
+            <q-icon icon="mdi-calendar-text-outline color-red6" :size="30"></q-icon>
+          </div>
+          <div class="font-12 mt-xs">
+            团操订单
           </div>
         </div>
-      </div>
-
-      <div class="flex-col mt-md elevation-4 bg-white bd-radius-20 pd-sm">
-        <div>
-          <!--          二维码-->
+        <div class="flex-1 col-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-white">
+          <div>
+            <q-icon icon="mdi-bullhorn color-orange5" :size="34"></q-icon>
+          </div>
+          <div class="font-12 mt-xs">
+            活动订单
+          </div>
         </div>
-        <div class="row-all-center">
-          <img class="size200" src="/static/img/qrcode.png"/>
-        </div>
+        <!--          <div class="flex-1 row-all-center py-10 bd-radius-10 mb-sm elevation-4 bg-red5 color-white">
+                    会员：200天
+                  </div>-->
       </div>
+    </div>
 
-      <div class="flex-col mt elevation-4 bg-white bd-radius">
-        <q-row-item>
-          课程预约
-          <q-icon icon="arrow-right" size="12" class="color-sub"></q-icon>
-        </q-row-item>
+
+    <div class="flex-col mt-md elevation-4 bg-white bd-radius-20 pd-sm">
+      <div>
+        <!--          二维码-->
       </div>
+      <div class="row-all-center">
+        <img class="size200" src="/static/img/qrcode.png"/>
+      </div>
+    </div>
+
+    <div class="flex-col mt elevation-4 bg-white bd-radius">
+      <q-row-item>
+        课程预约
+        <q-icon icon="arrow-right" size="12" class="color-sub"></q-icon>
+      </q-row-item>
     </div>
   </div>
 </template>
--- socialuni-js/packages/qing-ui/qing-ui/src/components/QRowItem.vue
+++ socialuni-js/packages/qing-ui/qing-ui/src/components/QRowItem.vue
@@ -1,20 +1,35 @@
 <template>
   <!--  可点击的行-->
-  <div class="q-box-between bg-click" :class="{'bb':border}" @click="click">
-    <slot></slot>
+  <div class="row-between-center" :class="{'bb':border}">
+    <slot name="title"></slot>
+    <div class="use-click flex-1" @click="click" :class="[hasTitle?'row-end-center':'row-between-center']">
+      <div>
+        <slot></slot>
+      </div>
+      <q-icon icon="arrow-right" size="12" class="color-sub flex-none"></q-icon>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import {Emit, Component, Vue, toNative, Prop} from 'vue-facing-decorator'
+import QIcon from "./QIcon.vue";
 
 //和row-line区别，这个有上下pd那个没有
 /*
 显示出来已经选了的城市，给她画上钩
 * */
 @toNative
-@Component({})
+@Component({
+  components: {QIcon}
+})
 export default class QRowItem extends Vue {
   @Prop({default: false, type: Boolean}) border: boolean
+
+  hasTitle = false
+
+  mounted() {
+    this.hasTitle = !!this.$slots.title
+  }
 
   @Emit()
   click() {
