9582de2f8e86540bc89f6420bb130f8b31100616
socialuni <socialuni@163.com>
2024-05-23 22:53:06 +0800

    修复联盟官网

--- socialuni-js/socialuni-official-ui/src/main.ts
+++ socialuni-js/socialuni-official-ui/src/main.ts
@@ -8,6 +8,7 @@ import JsonViewer from "vue3-json-viewer";
 import "vue3-json-viewer/dist/index.css";
 import '@mdi/font/css/materialdesignicons.min.css';
 import 'github-markdown-css/github-markdown.css'
+import 'uno.css'
 
 const app = createApp(App)
 
