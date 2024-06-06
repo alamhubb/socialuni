2016412e91cdc7da4742c492d8398848f3ce2102
socialuni <socialuni@163.com>
2024-06-07 07:57:58 +0800

    上传运营账户

--- socialuni-js/packages/socialuni-expand/socialuni-expand-sdk/src/service/SocialuniLikeService.ts
+++ socialuni-js/packages/socialuni-expand/socialuni-expand-sdk/src/service/SocialuniLikeService.ts
@@ -3,10 +3,12 @@ import {socialuniLikeConfigModule} from "../store/SocialuniLikeConfigModule";
 import QingAppUtil from "qingjs/src/util/QingAppUtil";
 import PlatformUtils from "socialuni-user-sdk/src/util/PlatformUtils";
 import SocialuniAppConfig from "socialuni-app-sdk/src/constant/SocialuniAppConfig";
+import UserMsgUtil from "socialuni-user-sdk/src/util/UserMsgUtil";
 
 export default class SocialuniLikeService {
     static async checkUserCoinAndPay(coinNum: number) {
         const defaultPayNum = 1
+        UserMsgUtil.unLoginMessage()
         if (socialuniAppUserModule.userCoinNum < coinNum) {
             await QingAppUtil.AlertUtil.confirm(`打招呼需要支付${coinNum}金币，您的金币不足，余额为：${socialuniAppUserModule.userCoinNum},是否充值金币`)
             await PlatformUtils.payCoin(defaultPayNum)
