b156e569f9b7698686fc6669ab446b7dc1683c4f
qinkaiyuan <qinkaiyuan94@163.com>
2024-09-07 00:45:22 +0800

    修复喜欢报错的问题

--- socialuni-server/socialuni-tance-dev/src/main/java/com/socialuni/social/tance/dev/config/SocialuniFeignAspect.java
+++ socialuni-server/socialuni-tance-dev/src/main/java/com/socialuni/social/tance/dev/config/SocialuniFeignAspect.java
@@ -11,16 +11,20 @@ import com.socialuni.social.common.api.utils.RequestUtil;
 import com.socialuni.social.common.api.dao.DO.RequestLogDO;
 import com.socialuni.social.common.sdk.utils.ErrorLogUtil;
 import com.socialuni.social.common.sdk.utils.RequestLogUtil;
+import com.socialuni.social.tance.dev.api.SocialuniNoUseFeignAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -48,24 +52,35 @@ public class SocialuniFeignAspect {
         // 获取目标对象的类
         Class<?> targetClass = target.getClass();
 
+        Boolean hasFlag = false;
+
+        SocialuniNoUseFeignAspect feignAspect = targetClass.getAnnotation(SocialuniNoUseFeignAspect.class);
+
+        if (feignAspect != null) {
+            hasFlag = true;
+        }
+        if (!hasFlag) {
+            // 获取方法签名
+            MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+            Method method = methodSignature.getMethod();
+            feignAspect = method.getAnnotation(SocialuniNoUseFeignAspect.class);
+
+            if (feignAspect != null) {
+                hasFlag = true;
+            }
+        }
+
         // 获取目标类实现的接口
         Class<?>[] interfaces = targetClass.getInterfaces();
 
-       /* System.out.println("目标类实现的接口: " + Arrays.toString(interfaces));
-
-        String feignTypeName = "org.springframework.cloud.openfeign.FeignClient";
-
         for (Class<?> interfaceClass : interfaces) {
-            System.out.println("接口 " + interfaceClass.getName() + " 上的注解:");
-            Annotation[] annotations = interfaceClass.getAnnotations();
-            for (Annotation annotation : annotations) {
-                String annotationTypeName = annotation.annotationType().getName();
-                if (feignTypeName.equals(annotationTypeName)) {
-                    System.out.println("  " + annotation.annotationType().getName());
-                    System.out.println("  " + annotation.getClass().getName());
+            FeignClient feignClient = interfaceClass.getAnnotation(FeignClient.class);
+            if (feignClient != null) {
+                if (hasFlag) {
+                    log.info("chufale you zidongchulide ");
                 }
             }
-        }*/
+        }
 
         // 执行目标方法
         Object result = joinPoint.proceed();
