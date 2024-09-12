30f1c965f6768984a83276857360e897931f1f09
socialuni <socialuni@163.com>
2024-09-13 05:28:53 +0800

    成功实现了依赖注入

--- socialuni-js/test-vite/src/plugins/TestSerivce.ts
+++ socialuni-js/test-vite/src/plugins/TestSerivce.ts
@@ -5,7 +5,7 @@ import type {TTTTAaaa} from "@/plugins/TypeIoc/Testaa";
 export default class TestSerivce {
 
     @Resource(22)
-    testA: TTTTAaaa
+    testA: TTTTAaaa = 20
 
     test() {
         console.log(11111)
--- socialuni-js/test-vite/src/test/fsafasdf.js
+++ socialuni-js/test-vite/src/test/fsafasdf.js
@@ -2,39 +2,11 @@
 const defineProperty = Object.defineProperty;
 const getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 
-// `applyDecorators` 函数：应用装饰器到对象属性
-function applyDecorators(decorators, target, propertyKey, condition) {
-    let descriptor = null
-    if (condition > 1) {
-        descriptor = undefined
-    } else {
-        console.log(1111)
-        console.log(getOwnPropertyDescriptor(target, propertyKey))
-        descriptor = (condition ? getOwnPropertyDescriptor(target, propertyKey) : target)
-    }
-    for (let i = decorators.length - 1; i >= 0; i--) {
-        const decorator = decorators[i];
-        if (decorator) {
-            descriptor = (condition ? decorator(target, propertyKey, descriptor) : decorator(descriptor)) || descriptor;
-        }
-    }
-
-    if (condition && descriptor) {
-        defineProperty(target, propertyKey, descriptor);
-    }
-
-    return descriptor;
-}
-
 // `definePropertyIfAbsent` 函数：在对象上定义属性（如果属性不存在）
 function definePropertyIfAbsent(object, key, value) {
-    if (!(key in object)) {
-        object[key] = value;
-    } else {
-        console.log(object)
-        console.log(key)
-        defineProperty(object, key, {enumerable: true, configurable: true, writable: true, value});
-    }
+    console.log(object)
+    console.log(key)
+    defineProperty(object, key, {enumerable: true, configurable: true, writable: true, value});
 }
 
 // 类定义
@@ -46,23 +18,6 @@ class MyClass {
     test() {
         console.log(11111);
     }
-}
-
-// 装饰器函数：简单打印和设置值
-function decoratorFunction(value) {
-    return function (target, propertyKey, descriptor) {
-        if (!descriptor) {
-            descriptor = {
-                enumerable: true,
-                configurable: true,
-                writable: true,
-                value: value
-            };
-        } else {
-            descriptor.value = value;
-        }
-        return descriptor;
-    };
 }
 
 console.log(MyClass.prototype)
